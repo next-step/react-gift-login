95492da713fc0357c6dcf76e515cef475e88d749
이병길 <qudrlf72@naver.com>
2025-06-24 04:04:08 +0900

    chore: Prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 function App() {
   return (
-    <div style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "100vw",
-      height: "100vh",
-    }}>
-      <div style={{fontSize: 30}}>Hello, world!</div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <div style={{ fontSize: 30 }}>Hello, world!</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
