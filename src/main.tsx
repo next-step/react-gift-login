164fd8b03e33133a58fa70f7917a10a8b5e3cd44
Jaeho <sjaeho348@gmail.com>
2025-06-24 02:46:22 +0900

    chore : emotion설치하고, main.tsx에 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import Reset from './styles/reset.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Reset />
     <App />
   </StrictMode>,
 )
