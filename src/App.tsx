939532eb011b9211c77af3426f7606e02f2865fa
daaoooy <y020d226y@daum.net>
2025-06-24 01:41:06 +0900

    refactor(app): 불필요한 코드 삭제 및 테스트 문구 작성
    App.tsx와 main.tsx 의 불필요한 코드를 제거하고 테스트 문구를 작성함

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Global } from '@emotion/react'
+import { globalStyle } from '@/styles/global'
+import styled from '@emotion/styled'
+
+// emotion test
+const Button = styled.button`
+  background-color: rgb(255, 244, 171);
+  color: black;
+  padding: 10px 20px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    background-color: rgb(255, 238, 106);
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Global styles={globalStyle} />
+      <div>테스트 중입니다. 불필요한 코드 삭제 완료.</div>
+      <div>testing..</div>
+      <Button>테스트 버튼</Button>
     </>
   )
 }
