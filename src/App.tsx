a200bcb180bb1d1085c0372d0f21d21743d78833
kimgho <kgh67811@knu.ac.kr>
2025-06-23 23:10:06 +0900

    feat(all): emotion library 추가
    - reset.ts를 통해 전역 스타일 초기화
    - CDN 방식으로 Pretendard 폰트 적용
    - styles 폴더 추가에 따라 alias 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { GlobalResetStyle } from "@styles/reset";
+import { Global } from "@emotion/react";
+
 const App = () => {
-  return <div>카카오테크캠퍼스 2단계</div>;
+  return (
+    <>
+      <Global styles={GlobalResetStyle} />
+      <div>카카오테크캠퍼스 2단계</div>
+    </>
+  );
 };
 
 export default App;
