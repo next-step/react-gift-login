5e7f37e40fce98b0f7cbae98b6e0be5ae3ef7e5c
손정훈 <129269335+SonJH7@users.noreply.github.com>
2025-06-24 22:27:01 +0900

    부산대 FE 손정훈 1단계 - 프로젝트 세팅 (#52)
    * docs(README): step1 ## 구현할 기능 목록
    
    * chore(all) : 불필요한 파일/코드 정리
    
    * chore(all) : 절대경로로 import alias 설정
    
    * chore(all) : prettier 설치
    
    * feat(all) : alias 설정 추가
    
    * chore(all) : prettier 관련 파일 추가
    
    * chore(all) : emotion 스타일 라이브러리 및 reset css 추가
    
    * chore(all) : 기본 폰트로 Pretendard 적용

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,48 @@
+﻿import { Global, css } from "@emotion/react";
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      *,
+      *::before,
+      *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+      }
+      html,
+      body,
+      #root {
+        height: 100%;
+      }
+      body {
+        line-height: 1;
+        -webkit-font-smoothing: antialiased;
+        font-family: 'Pretendard', sans-serif;
+
+      }
+      ol,
+      ul,
+      menu {
+        list-style: none;
+      }
+      blockquote,
+      q {
+        quotes: none;
+      }
+      blockquote::before,
+      blockquote::after,
+      q::before,
+      q::after {
+        content: "";
+        content: none;
+      }
+      table {
+        border-collapse: collapse;
+        border-spacing: 0;
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
