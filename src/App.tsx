b8fadcc93dad476317d0bd91c91cc51d75a669c7
shushuburger <03robin@naver.com>
2025-06-24 11:26:33 +0900

    chore(config): 절대 경로 import를 위한 alias 설정
    - 절대 경로 사용 위해서 path 설치하였습니다.
    - vite.config.ts와 tsconfig.app.json에 각각 @와 src를 연관지어 절대경로로 생각하도록 코드 작성해주었습니다.
    - 임의로 assets에 logo.png를 추가하여 App.tsx에서 호출시 잘 되는 것을 확인하였습니다.
    - Readme에 해당 기능 구현 완료 체크하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Logo from '@/assets/logo.png';
+
 function App() {
   return (
     <div>
       <h2>불필요한 코드 삭제 후 App 화면</h2>
       <h2>Complete!</h2>
+      <img src={Logo} alt="로고" width={100} />
     </div>
   );
 }
