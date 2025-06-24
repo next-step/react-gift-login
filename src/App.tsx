fd21fbea384c80dd9420f35e0a86348a2df5f37e
ppre1ude <cjh5110@naver.com>
2025-06-24 16:42:06 +0900

    feat: emotion library 추가 및 reset css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <div>카카오테크캠퍼스 Frontend 2단계</div>;
-};
+import { Global, css } from '@emotion/react';
+import reset from './styles/reset';
+
+function App() {
+  // ...
+  return (
+    <>
+      <Global
+        styles={css`
+          ${reset}
+        `}
+      />
+      {<div>카카오테크캠퍼스 Frontend 2단계</div>}
+    </>
+  );
+}
 
 export default App;
