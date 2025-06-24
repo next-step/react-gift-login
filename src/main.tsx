9c605a4b6361fe7994f346bacbf298f525e6040a
kimdowan <kimdowan1004@naver.com>
2025-06-24 14:27:14 +0900

    emotion 스타일 라이브러리 추가, reset css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@/index.css';
-import App from '@/App.tsx';
+import App from '@/App';
+import { GlobalStyles } from '@/styles/global';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>
 );
