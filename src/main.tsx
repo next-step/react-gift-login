7125ed3053baa3df277368f7392b67c59949c96a
HOJUN_LEE <leehojun.full@gmail.com>
2025-06-24 22:59:26 +0900

    충남대 FE 이호준 - 1단계 프로젝트 세팅 (#36)
    * 1. 불필요한 파일, 코드 정리
    - app.tsx 리턴 파일 초기화
    - app.css 파일 내용 삭제
    - index.css 파일 내용 삭제
    
    * 2. alias 추가
    - tsconfig.app.json 수정
    - vite.config.ts 수정
    - @types/node install
    
    * 3. prettier 추가
    - prettier install
    - .prettierrc 파일 생성 및 수정
    - package.json script 추가
    - npm run format으로 오류 탐지
    - npm run format:fix로 오류 탐지 및 바로 수정
    
    * emotion 스타일 라이브러리 추가
    - reset css 적용
    - App.tsx에 reset css가 적용되어 반환
    - reset.css 내용은 https://meyerweb.com/eric/tools/css/reset/ 참고
    
    * 5. Pretendard 기본 폰트 적용
    - reset.css 파일에 폰트 관련 코드 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { reset } from '@styles/reset.ts'
+import { Global } from '@emotion/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global styles={reset} />
     <App />
   </StrictMode>,
 )
