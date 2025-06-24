62a42de3903976f53ef90451347ae97e244ebc00
Jiwon Jeong <81281798+jjw5655@users.noreply.github.com>
2025-06-24 15:21:34 +0900

    chore: Prettier 코드 포맷터 추가 및 기본 설정

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const TestComponent: React.FC = () => {
   return (
     <div>
       <p>This is a test component using absolute path import!</p>
     </div>
-  )
-}
+  );
+};
 
-export default TestComponent 
+export default TestComponent; 
