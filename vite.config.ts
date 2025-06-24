1d7646c265818d2da6cf1cf0423519ce11578667
younghyunChoi <lewis177777@gmail.com>
2025-06-24 21:24:52 +0900

    전남대 FE 최영현 - 1단계 프로젝트 세팅 (#58)
    * alias 설정
    
    * prettier 설정
    
    * alias styles도 추가
    
    * emotion 스타일 라이브러리 추가 reset csswjrdyd
    
    * 기본폰트 pretend 적용
    
    * 불필요한 파일 정리
    
    * alias왜 안될까요...

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') }, // 절대 경로로 설정
+      { find: '@styles', replacement: resolve(__dirname, 'src/styles') }, // 절대 경로로 설정
+    ],
+  },
+  plugins: [react(), tsconfigPaths()],
+});
