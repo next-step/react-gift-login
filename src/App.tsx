a44b10be7dce4a7f3b3a5592679ba48acc178df4
cktjgus <cktjgus04@naver.com>
2025-06-25 01:11:35 +0900

    emotion 스타일 라이브러리 추가 및 reset css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
+import { Global } from '@emotion/react';
+import { resetStyle } from '@/styles/reset';
 
 function App() {
   return (
     <>
+      <Global styles={resetStyle} /> 
       <h1>Hello React</h1>
     </>
   )
