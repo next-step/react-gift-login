0e9c0c3fae11f3e4f37e03413ffc0ef26f7c4b1f
mickey7 <115975877+aaaaaattt@users.noreply.github.com>
2025-06-24 20:45:05 +0900

    전남대 FE 최혁 1단계 - 프로젝트 세팅 (#6)
    * 불필요한 파일, 코드 정리
    
    * alias 설정
    
    * Prettier 설정
    
    * resetCSS 추가
    
    * emotion 스타일 라이브러리 추가
    
    * 기본 폰트 pretendard 추가
    
    ---------
    
    Co-authored-by: ch <chlgur13m@naver.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './reset.css';
 import App from './App.tsx'
+import 'pretendard/dist/web/static/pretendard.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
