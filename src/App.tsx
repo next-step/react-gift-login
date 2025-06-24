9624db54ba5ab77f09498dceef2307ec5a0c7839
jinhyun <109673353+jinhyun71744@users.noreply.github.com>
2025-06-25 01:11:14 +0900

    전남대 FE 박진현 - 1단계 프로젝트 세팅 (#42)
    * chore: 초기 세팅 및 불필요한 파일 제거
    
    - 불필요한 이미지 제거 (react.svg등)
    - App.tsx, main.tsx 코드 정리
    - 불필요한 파일 제거
    - README.md에 기능 추가
    
    * chore: 절대 경로 alias 설정
    
    - @components, @pages, @styles 등 alias 설정
    - vite.config.ts, tsconfig.app.json 수정
    - TypeScript용 타입 정의 추가
    
    * chore: prettier 설치 및 기본 설정
    
    - prettier 설치
    - 기본 옵션 설정
    
    * style: emotion 설치 및 App 컴포넌트에 reset css 적용
    
    - emotion 설치
    - emotion의 Global 컴포넌트를 사용해 reset.css 적용
    
    * style: 기본폰트를 Pretendard로 설정
    
    - CDN으로 Pretendard 웹폰트 불러오기
    - body에 font-family 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,158 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Global, css } from '@emotion/react';
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Global
+        styles={css`
+          @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css');
 
-export default App
+          html,
+          body,
+          div,
+          span,
+          applet,
+          object,
+          iframe,
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6,
+          p,
+          blockquote,
+          pre,
+          a,
+          abbr,
+          acronym,
+          address,
+          big,
+          cite,
+          code,
+          del,
+          dfn,
+          em,
+          img,
+          ins,
+          kbd,
+          q,
+          s,
+          samp,
+          small,
+          strike,
+          strong,
+          sub,
+          sup,
+          tt,
+          var,
+          b,
+          u,
+          i,
+          center,
+          dl,
+          dt,
+          dd,
+          ol,
+          ul,
+          li,
+          fieldset,
+          form,
+          label,
+          legend,
+          table,
+          caption,
+          tbody,
+          tfoot,
+          thead,
+          tr,
+          th,
+          td,
+          article,
+          aside,
+          canvas,
+          details,
+          embed,
+          figure,
+          figcaption,
+          footer,
+          header,
+          hgroup,
+          menu,
+          nav,
+          output,
+          ruby,
+          section,
+          summary,
+          time,
+          mark,
+          audio,
+          video {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font-size: 100%;
+            font: inherit;
+            vertical-align: baseline;
+          }
+
+          article,
+          aside,
+          details,
+          figcaption,
+          figure,
+          footer,
+          header,
+          hgroup,
+          menu,
+          nav,
+          section {
+            display: block;
+          }
+
+          body {
+            line-height: 1;
+            font-family:
+              'Pretendard',
+              -apple-system,
+              BlinkMacSystemFont,
+              system-ui,
+              Roboto,
+              'Helvetica Neue',
+              'Segoe UI',
+              'Apple SD Gothic Neo',
+              'Noto Sans KR',
+              'Malgun Gothic',
+              sans-serif;
+          }
+
+          ol,
+          ul {
+            list-style: none;
+          }
+
+          blockquote,
+          q {
+            quotes: none;
+          }
+
+          blockquote:before,
+          blockquote:after,
+          q:before,
+          q:after {
+            content: '';
+            content: none;
+          }
+
+          table {
+            border-collapse: collapse;
+            border-spacing: 0;
+          }
+        `}
+      />
+      <h1>app</h1>
+      <p>글꼴 확인</p>
+    </>
+  );
+};
+
+export default App;
