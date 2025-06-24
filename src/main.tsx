fc55a48e31329cae2a007a14eb6318d0308f3bd1
Chahyun Lee <163325051+chahyunlee@users.noreply.github.com>
2025-06-24 23:09:17 +0900

    부산대 FE 이차현 - 1단계 프로젝트 세팅 (#39)
    * chore : 불필요한 파일, 코드 정리
    
    * chore: 절대경로 설정  및 상대경로 -> 절대경로 변경
    
    * chore: Prettier 설치
    
    * chore: emotion 스타일 라이브러리 추가 및 reset css 적용
    
    * style: Pretendard 폰트 설치 및 전역 폰트 설정
    
    * style: reset 스타일 분리 및 global 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@/index.css'
+import App from '@/App'
+import 'pretendard/dist/web/static/pretendard.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
