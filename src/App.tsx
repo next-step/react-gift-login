276a82e91900eca1def6e1fa306d416c5d724635
changhee <changhee@kangwon.ac.kr>
2025-06-25 23:22:23 +0900

    feat: 선물 테마 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import Background from '@src/components/Background'
-import Center from '@src/components/Center'
+import Background, {Inner} from '@src/components/Background'
 import Title from '@src/components/Title'
 import Banner from '@src/components/Banner'
+import Maintheme from '@src/components/Maintheme'
 function App() {
   return (
     <>
       <Background>
-        <Center>
+        <Inner>
           <header>
             <Title />
           </header>
           <main>
             <Banner />
+            <Maintheme />
           </main>
-        </Center>
+        </Inner>
       </Background>
     </>
   )
