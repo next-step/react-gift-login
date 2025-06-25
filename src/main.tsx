2c1b8410dc7e03df61c1513c9aa7412c3e206a1b
Chahyun Lee <163325051+chahyunlee@users.noreply.github.com>
2025-06-25 15:05:49 +0900

    fix: eol 이슈 수정 및 import 순서 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import 'pretendard/dist/web/static/pretendard.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import App from '@/App'
-import 'pretendard/dist/web/static/pretendard.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
