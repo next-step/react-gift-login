006e4f9b517872b3c13457a1c848b816534c91c6
Jaeho <162589155+Jaeho-Site@users.noreply.github.com>
2025-06-25 10:28:05 +0900

    충남대 FE 신재호 1단계 - 프로젝트 세팅  (#10)
    * docs : README.md에 구현기능목록 정리
    
    * chore : 불필요한 파일, 코드 정리
    
    * fix : ts설정오류 수정 - erasableSyntaxOnly삭제
    
    * chore : alias 절대경로 설정
    
    * chore : prettier설정 추가
    
    * chore : emotion설치하고, main.tsx에 적용
    
    * style : Pretendard를 기본폰트로 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import Reset from './styles/reset.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Reset />
     <App />
   </StrictMode>,
 )
---
+++ src/styles/reset.tsx
@@ -0,0 +1,22 @@
+import { Global, css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
+
+export default function Reset() {
+  return (
+    <Global
+      styles={css`
+        ${emotionReset}
+        *, *::after, *::before {
+          box-sizing: border-box;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+        }
+        body {
+          font-family: 'Pretendard', Arial, sans-serif;
+          background-color: #fff;
+        }
+      `}
+    />
+  );
+}
