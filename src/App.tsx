057ac77cd7960baff03189973e781bc5f3d6bb29
kimdowan <kimdowan1004@naver.com>
2025-06-25 14:39:29 +0900

    1. 이전단계 피드백 수용(ko) 및 emotion에 Color, Tyography 토큰 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  padding: ${({ theme }) => theme.spacing.spacing6};
+  background-color: ${({ theme }) => theme.colors.semantic.backgroundFill};
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.semantic.textDefault};
+  font: ${({ theme }) => theme.typography.title1Bold};
+  margin-bottom: ${({ theme }) => theme.spacing.spacing4};
+`;
+
+const Subtitle = styled.h2`
+  color: ${({ theme }) => theme.colors.semantic.textSub};
+  font: ${({ theme }) => theme.typography.subtitle1Regular};
+  margin-bottom: ${({ theme }) => theme.spacing.spacing4};
+`;
+
+const Warning = styled.div`
+  background-color: ${({ theme }) => theme.colors.semantic.criticalBackground};
+  color: ${({ theme }) => theme.colors.semantic.critical};
+  padding: ${({ theme }) => theme.spacing.spacing4};
+  border-radius: 8px;
+  font: ${({ theme }) => theme.typography.body1Regular};
+`;
+
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <Container>
+      <Title>제목 테스트</Title>
+      <Subtitle>테스트</Subtitle>
+      <Warning>에러 테스트</Warning>
+    </Container>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
 import { GlobalStyles } from '@/styles/global';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './styles/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyles />
-    <App />
-  </StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
 );
