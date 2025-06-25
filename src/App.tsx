b9095761b6ee7bdb6eec1489719f1b55dbc076e6
hyemin <hyemin6647@naver.com>
2025-06-25 15:56:04 +0900

    style: emotion에 Color, Typography 토큰  설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import img from 'assets/react.svg'
-import GlobalStyles from 'assets/GlobalStyles'
+import GlobalStyles from '@/styles/GlobalStyles'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './styles/theme/index';
 
 function App() {
 
   return (
-    <>
-    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <img src={img} alt="React logo" />
       <h1>Hello Vite + React!</h1>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
 export default App
