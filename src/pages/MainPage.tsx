d1b2fd922b928374be816f010839aab678599685
Moderator11 <p7925221@gmail.com>
2025-06-26 02:56:59 +0900

    feat: Add login page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,5 @@
 import CheerUpPanel from "@src/components/CheerUpPanel";
 import GiftThemePanel from "@src/components/GiftThemePanel";
-import NavBar from "@src/components/NavBar";
 import RealTimeRankPanel from "@src/components/RealTimeRankPanel/RealTimeRankPanel";
 import RecipientSelector from "@src/components/RecipientSelector";
 
