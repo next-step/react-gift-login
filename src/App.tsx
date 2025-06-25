60011aa7fe43a74f9f8792550107a8ab3d77168c
dib3474 <dib3474@gmail.com>
2025-06-25 13:44:58 +0900

    refactor: PR review 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import GlobalStyle from "@/styles/GlobalStyle"
+import GlobalStyle from "@/styles/GlobalStyle";
 
 function App() {
-
   return (
     <>
       <GlobalStyle />
       <h1>Pretendard</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
