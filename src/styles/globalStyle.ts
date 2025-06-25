39270903331e9d19f1fd5edc4a7e2ba9aa0d9c12
Jeong Joonmo <waysoleward01@naver.com>
2025-06-25 13:34:13 +0900

    충남대 FE 정준모 - 1단계 프로젝트 세팅  (#30)
    * feat: 불필요한 파일 및 코드 정리
    
    * feat: prettier 설정 추가
    
    * feat: import 관련 alias 수정
    
    * feat: emotion 라이브러리 적용
    
    * feat: Pretandard 폰트 적용
    
    * feat: 코드 리뷰 반영
    
    * refactor: 불필요한 import 제거

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,11 @@
+import { css } from "@emotion/react";
+
+export const globalStyle = css`
+  html,
+  body {
+    font-family: "Pretendard Variable", sans-serif;
+    background-color: #fff;
+    color: #000;
+    line-height: 1.6;
+  }
+`;
