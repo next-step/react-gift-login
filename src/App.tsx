843cb7e174ed7b510758b47c8a327b3444ba22e2
aaaaaattt <choigur03@gmail.com>
2025-06-26 07:43:48 +0900

    feat : mobile first design 기반의 Layout 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./theme"; // 방금 정의한 테마 파일
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <></>
+      <Layout>test</Layout>
     </ThemeProvider>
   );
 }
