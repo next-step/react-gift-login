2c4a79f0a64f62c41af17c07bfaa0e0fabf76471
cheog0 <https://github.com/cheog0>
2025-06-25 11:56:02 +0900

    feat: Emotion 스타일 라이브러리 추가 및reset CSS 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react';
-import { theme } from '@/styles/theme';
-import App from './App';
-import './styles/reset.css';
-import './index.css';
+import Mainpage from '@/pages/Mainpage';
 
-createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
-);
+function App() {
+  return <Mainpage />;
+}
+
+export default App;
