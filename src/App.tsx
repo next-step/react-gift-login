9ff832576204eb6552137fb04066dd4c1d216d31
Jiwon Jeong <81281798+jjw5655@users.noreply.github.com>
2025-06-26 02:50:41 +0900

    refactor: 불필요한 React import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styled from '@emotion/styled'
 import TestComponent from '@/components/TestComponent'
 import { Button, SecondaryButton } from '@/components/StyledButton'
