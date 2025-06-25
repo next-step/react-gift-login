e332554be0fb105d94f90d7df43376df60c3b970
LeeHeeSung98989898 <goqpqp234@gmail.com>
2025-06-25 15:50:08 +0900

    feat: emotion에 Color, Typography, Spacing 토큰 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import theme from '@/styles/theme/theme.ts'
 // import './index.css';
 import App from './App.tsx';
 import 'pretendard/dist/web/static/pretendard.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
