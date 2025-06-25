18c6221a9d89e724e89a9903dc47f153d5ef4b4d
aaaaaattt <choigur03@gmail.com>
2025-06-26 02:47:26 +0900

    emotion 라이브러리 추가 후 Color, Typhgraphy 토큰 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme"; // 방금 정의한 테마 파일
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <></>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
