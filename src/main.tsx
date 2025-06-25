8bb9ad209fde2bedac67b8d4f2a8a5ad2b1dd478
SeHIgh <128220943+SeHIgh@users.noreply.github.com>
2025-06-26 00:39:29 +0900

    경북대 FE 장세현 1단계 - 프로젝트 세팅 (#15)
    * docs(readme): README.md 에 구현할 기능 목록 정리
    
    * chore: 불필요한 파일, 코드 정리 및 커밋 메시지 템플릿 작성
    
    * chore: 절대경로로 Import 할 수 있도록 alias 설정
    
    * chore(prettier): Prettier 설정 추가 및 eslint 충돌 예방 설정 추가
    
    * feat(styles): emotion 스타일 라이브러리 추가, reset css 적용, Pretendard 폰트 적용
    
    * refactor(styles): 스타일 파일 상대 경로 -> 절대 경로 수정
    
    * style(prettier, font): 코드 포맷팅 적용 및 외부 CDN 의존성 제거 후 번들링에 포함
    
    ---------
    
    Co-authored-by: SeHIgh <sehi0119@naver.comsehi0119@naver.comsehi0119@naver.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { css, Global } from '@emotion/react'
+import { resetStyles } from '@/styles/reset'
+import { fontStyles } from '@/styles/font'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* Global 스타일 적용 */}
+    <Global
+      styles={css`
+        ${resetStyles}
+        ${fontStyles}
+      `}
+    />
+    {/* App 컴포넌트 렌더링 */}
     <App />
   </StrictMode>,
 )
