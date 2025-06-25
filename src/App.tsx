7e24d972986aae1df7979c9eb08f6c6f243a1864
Changmok LEE <cmlee5075@gmail.com>
2025-06-25 21:44:39 +0900

    chore: App.tsx에서 ThemeProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import Navigation from './layouts/navigation';
-import GiftsPage from './pages/GiftsPage';
-import GlobalStyle from './styles/global';
+import Navigation from '@/components/Navigation';
+import GiftsPage from '@/pages/GiftsPage';
+import GlobalStyle from '@/styles/global';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from '@/styles/theme';
+import styled from '@emotion/styled';
+
+const AppContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: ${({ theme }) => theme.colors.semantic.background.fill};
+`;
+
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Navigation />
-      <GiftsPage />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <AppContainer>
+          <Navigation />
+          <GiftsPage />
+        </AppContainer>
+      </ThemeProvider>
     </>
   );
 }
