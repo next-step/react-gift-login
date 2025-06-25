601e0bcc036bccc20aa46d65bba879593f1ea81e
Chahyun Lee <163325051+chahyunlee@users.noreply.github.com>
2025-06-25 15:32:07 +0900

    feat: 디자인 토큰 생성 및 ThemeProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import 'pretendard/dist/web/static/pretendard.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@emotion/react'
+import theme from '@/styles/theme'
 import '@/index.css'
 import App from '@/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme = { theme }>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
