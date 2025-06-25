34deeff46e11e9ad70a0b576012b10a012f3ee61
SeHigh <sehi0119@naver.com>
2025-06-26 01:42:34 +0900

    feat(router): 추후 페이지 라우팅을 위한 Router 추가
    - react-router-dom 라이브러리 설치
    - src/App.tsx 수정
    - src/pages/Home.tsx 추가
    - src/shared/Router.tsx 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-function App() {
-  return (
-    <>
-      <h1>카카오테크 캠퍼스 2단계 - 1차 과제</h1>
-      <h2>카카오 선물하기 로그인</h2>
-    </>
-  )
+import Router from './shared/Router'
+
+const App = () => {
+  return <Router />
 }
 
 export default App
