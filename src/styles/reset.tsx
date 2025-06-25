e4fa0a049f88141d61284925cea9f0881cc95062
Jaeho <sjaeho348@gmail.com>
2025-06-25 14:46:07 +0900

    chore: 1단계 리뷰 반영 및 2단계  README.md 추가

--- src/styles/reset.tsx
+++ src/styles/reset.tsx
@@ -1,21 +1,25 @@
 import { Global, css } from '@emotion/react';
 import emotionReset from 'emotion-reset';
 
+const customGlobalStyles = css`
+  *, *::after, *::before {
+    box-sizing: border-box;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    font-smoothing: antialiased;
+  }
+  body {
+    font-family: 'Pretendard', Arial, sans-serif;
+    background-color: #fff;
+  }
+`;
+
 export default function Reset() {
   return (
     <Global
       styles={css`
         ${emotionReset}
-        *, *::after, *::before {
-          box-sizing: border-box;
-          -moz-osx-font-smoothing: grayscale;
-          -webkit-font-smoothing: antialiased;
-          font-smoothing: antialiased;
-        }
-        body {
-          font-family: 'Pretendard', Arial, sans-serif;
-          background-color: #fff;
-        }
+        ${customGlobalStyles}
       `}
     />
   );
