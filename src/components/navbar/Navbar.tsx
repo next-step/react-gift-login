82f7a1d68037e5b1c5b213b963841feccd0fd5a7
hyemin <hyemin6647@naver.com>
2025-06-26 16:58:11 +0900

    feat: navbar 아이콘 수정

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { css, useTheme } from "@emotion/react";
 const Navbar = () => {
   const theme = useTheme();
@@ -14,9 +12,42 @@ const Navbar = () => {
         background-color: ${theme.colors.gray};
       `}
     >
-      <div>아이콘</div>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.8"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-chevron-left"
+          aria-hidden="true"
+        >
+          <path d="m15 18-6-6 6-6"></path>
+        </svg>
+      </div>
       <div>선물하기</div>
-      <div>홈 아이콘</div>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-user-round"
+          aria-hidden="true"
+        >
+          <circle cx="12" cy="8" r="5"></circle>
+          <path d="M20 21a8 8 0 0 0-16 0"></path>
+        </svg>
+      </div>
     </div>
   );
 };
