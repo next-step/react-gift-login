d85b3a5448e98d1295f07d4f5136798a248f87f6
김성진 <gimseongjin@gimseongjin-ui-noteubug.local>
2025-06-26 12:52:53 +0900

    feat: 상단 네비게이션 바 영역 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/GlobalStyle';
 import Layout from './components/Layout/Layout';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Layout>
-        <div>check</div>
+        <NavigationBar />
+        <div></div>
       </Layout>
     </ThemeProvider>
   );
