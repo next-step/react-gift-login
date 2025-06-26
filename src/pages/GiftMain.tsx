0ee82c254974002cc819b75dd022d12a007a6b95
jiwonWP <bread0264@gmail.com>
2025-06-26 12:21:09 +0900

    feat: 카테코리 섹션 구현

--- src/pages/GiftMain.tsx
+++ src/pages/GiftMain.tsx
@@ -1,11 +1,14 @@
 import { PageContainer } from '@/components/layout/PageContainer';
 import { Navigation } from '@/components/header/Navigation';
+import { CategorySection } from '@/components/category/CategorySection';
 
 
 const GiftMain= () =>{
     return (
         <PageContainer>
             <Navigation />
+            <CategorySection />
+
             <h1>카카오 선물하기 메인 페이지</h1>
         </PageContainer>
     );
