89ab5143022a904daa1874ab97c9b0f43d0683ca
thgus0 <sohyun10521052@gmail.com>
2025-06-26 15:36:06 +0900

    chore: 상단 네이게이션 바 영역 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import '@/App.css'
+import { Navbar } from '@/components/Navbar'
 
 function App() {
   return (
-    <div className="app-container">
-      <h1>Vite + React</h1>
-      <p>리액트 프로젝트가 실행되었습니다.</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
