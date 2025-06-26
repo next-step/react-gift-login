30e04df499aa97e20452bcf3973d9b8d980ec72d
jabin00123 <133005789+jabin00123@users.noreply.github.com>
2025-06-27 01:10:47 +0900

    경북대 FE 윤자빈 - 1단계 프로젝트 세팅  (#55)
    * 1. 불필요한 파일, 코드 정리
    
    * 2. 절대경로 alias 설정
    
    * 구현할 기능 목록 README에 정리
    
    * 3. Prettier 설정 추가
    
    * 4. emotion 설정 및 reset css 적용
    
    * 5. Pretendard 기본 폰트 적용
    
    * 1. Emotion 적용 & Pretendard 전역 폰트 설정
    
    * 2. __dirname

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,37 @@
+/** @jsxImportSource @emotion/react */
+import { css, Global } from '@emotion/react'
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css');
+
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      html,
+      body {
+        height: 100%;
+        font-family: 'Pretendard Variable', 'Pretendard', system-ui, sans-serif;
+        background-color: #ffffff;
+        color: #000000;
+      }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+      button {
+        font-family: inherit;
+        background: none;
+        border: none;
+        cursor: pointer;
+      }
+    `}
+  />
+)
+
+export default GlobalStyle
