300b80aee70f38fd6f69f69f237e071d504331c8
toothlessdev <toothless042@gmail.com>
2025-06-26 21:33:57 +0900

    feat: GiftCard 컴포넌트 및 스타일 추가

---
+++ src/entities/gift/ui/GiftCard.tsx
@@ -0,0 +1,38 @@
+import type { PropsWithChildren } from "react";
+
+import * as Styles from "./GiftCard.styled";
+import type { GiftModel } from "@/entities/gift/model/GiftModel";
+import { VerticalSpacing } from "@/widgets/layouts/Spacing.styled";
+
+export const GiftCardGrid = ({ children }: PropsWithChildren) => {
+    return <Styles.Grid>{children}</Styles.Grid>;
+};
+
+export interface GiftCardProps extends GiftModel {
+    index: number;
+    isHighlighted?: boolean;
+}
+
+export const GiftCard = ({
+    index,
+    id,
+    name,
+    imageURL,
+    price,
+    brandInfo,
+    isHighlighted,
+}: GiftCardProps) => {
+    return (
+        <Styles.Wrapper key={id}>
+            <Styles.IndexLabel isHighlighted={isHighlighted}>{index}</Styles.IndexLabel>
+            <Styles.Image src={imageURL}></Styles.Image>
+            <VerticalSpacing size="12px" />
+
+            <Styles.Container>
+                <Styles.BrandLabel>{brandInfo.name}</Styles.BrandLabel>
+                <Styles.MenuLabel>{name}</Styles.MenuLabel>
+                <Styles.PriceLabel>{price.sellingPrice} 원</Styles.PriceLabel>
+            </Styles.Container>
+        </Styles.Wrapper>
+    );
+};
