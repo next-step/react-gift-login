a52c7f015f6d3c0b0a4bca87a8d772718793784e
Taewoong Heo <chamjoeun0111@gmail.com>
2025-06-26 10:58:49 +0900

    refactor: EventBanner.tsx 에서 스타일을 별도의 파일로 분리

--- src/components/EventBanner/EventBanner.tsx
+++ src/components/EventBanner/EventBanner.tsx
@@ -1,38 +1,9 @@
-import styled from '@emotion/styled';
-
-const EventBannerSection = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BannerCard = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: start;
-  flex-direction: column;
-
-  width: 95%;
-  height: 4rem;
-
-  box-sizing: border-box;
-  padding: 1rem;
-  gap: 5px;
-
-  background-color: ${({ theme }) => theme.colors.brand.kakaoYellow};
-  border-radius: ${({ theme }) => theme.borderRadius.md};
-`;
-
-const SectionTitle = styled.p`
-  font-size: ${({ theme }) => theme.typography.label.label1Bold.fontSize};
-  font-weight: ${({ theme }) => theme.typography.label.label1Bold.fontWeight};
-`;
-
-const SectionSubTitle = styled.p`
-  font-size: ${({ theme }) => theme.typography.label.label2Regular.fontSize};
-  font-weight: ${({ theme }) => theme.typography.label.label2Regular.fontWeight};
-  color: ${({ theme }) => theme.colors.gray[700]};
-`;
+import {
+  BannerCard,
+  EventBannerSection,
+  SectionSubTitle,
+  SectionTitle,
+} from './EventBanner.styles';
 
 function EventBanner() {
   return (
