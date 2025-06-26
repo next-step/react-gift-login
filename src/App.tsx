ef3df2ceaff890e824fe42057f52a12ee8a145ea
SonJH7 <sjunh02@naver.com>
2025-06-26 16:35:21 +0900

    feat(NavBar) : NavBar 컴포넌트 구현
    - 뒤로 가기 버튼, 로고 사진, 로그인 아이콘 적용 및 버튼 활성화
    - Layout에 모아 App에 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Layout from '@/Layout'
+import logo from '@/assets/logo.png'
 
 function App() {
   return (
-    <Layout>
+    <Layout logoSrc={logo}>
       <p>Hello, world!</p>
     </Layout>
   )
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,14 +1,25 @@
 ﻿import type { PropsWithChildren } from 'react'
 import styled from '@emotion/styled'
+import NavBar from '@/components/NavBar'
 
 const Container = styled.div`
   max-width: 720px;
   margin: 0 auto;
   padding: 0 16px;
 `
+  interface LayoutProps extends PropsWithChildren {
+  onBack?: () => void
+  logoSrc: string
+  onLoginClick?: () => void
+ }
 
-const Layout = ({ children }: PropsWithChildren) => {
-  return <Container>{children}</Container>
+const Layout = ({ children, onBack, logoSrc, onLoginClick }: LayoutProps) => {
+  return (
+    <Container>
+      <NavBar onBack={onBack} logoSrc={logoSrc} onLoginClick={onLoginClick} />
+      {children}
+    </Container>
+  )
 }
 
 export default Layout
