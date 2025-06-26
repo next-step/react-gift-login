156bceae410452f6bce3a5b38c7093c1098a545e
HanseKim <kimbird2002@naver.com>
2025-06-26 16:35:40 +0900

    feat: 성별 분류 UI 완성

--- src/components/Container/SexContainer.tsx
+++ src/components/Container/SexContainer.tsx
@@ -1,13 +1,28 @@
 import SexItem from '@/components/SexItem';
+import { useState } from 'react';
+
+import styled from '@emotion/styled';
+
+export const SexContainerWrapper = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 20px;
+`;
 
 function SexContainer() {
+  const [selectSex, setSelectsex] = useState<string>('All');
+  function handleSelect(sex: string) {
+    setSelectsex(sex);
+  }
   return (
-    <ul>
-      *성별 분류
-      <SexItem />
-      <SexItem />
-      <SexItem />
-    </ul>
+    <SexContainerWrapper>
+      <SexItem sex="All" selectSex={selectSex} onClick={() => handleSelect('All')} />
+      <SexItem sex="남성" selectSex={selectSex} onClick={() => handleSelect('남성')} />
+      <SexItem sex="여성" selectSex={selectSex} onClick={() => handleSelect('여성')} />
+      <SexItem sex="청소년" selectSex={selectSex} onClick={() => handleSelect('청소년')} />
+    </SexContainerWrapper>
   );
 }
 
