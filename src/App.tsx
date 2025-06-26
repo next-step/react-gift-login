9adc077838b5a20c096fe064a16e3379d9d71c7d
s202055564 <hcan1223@pusan.ac.kr>
2025-06-26 15:03:10 +0900

    1차 리뷰후 수정
    1.폰트 적용방식을 CDN -> CSS 방식으로 변경  index.html 수정 reset.ts 수정
    2.prettier vscode extion 으로 설치, 기존 pakage.json 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
       <>
         <GlobalStyle />
         <div>테스트용 for test</div>
+        <div>세상에 이런 폰트가 나오다니 천재인듯</div>
 
       </>
   )
--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -3,6 +3,21 @@ import { css } from '@emotion/react'
 
 const reset = css`
     /* Reset CSS */
+
+    @font-face {
+        font-family: 'Ownglyph_ParkDaHyun';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2411-3@1.0/Ownglyph_ParkDaHyun.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: 'Pretendard-Regular';
+        src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+        font-weight: 400;
+        font-style: normal;
+    }
+
     * {
         margin: 0;
         padding: 0;
@@ -10,9 +25,7 @@ const reset = css`
     }
 
     html, body {
-        font-family: 'PretendardVariable', Pretendard, -apple-system, BlinkMacSystemFont,
-        system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR',
-        'Malgun Gothic', sans-serif;
+        font-family: 'Pretendard-Regular', sans-serif;
     }
 `
 
