28ce08d4e2d4f90c8759db965909978e66175c37
bootaeng <tgwon0947@gmail.com>
2025-06-26 14:58:05 +0900

    style(layout): 네비게이션 바 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { Global } from '@emotion/react'
+import { Global, ThemeProvider } from '@emotion/react'
 import { globalReset } from '@/styles/reset'
-import { ThemeProvider } from '@emotion/react'
-import { Layout } from '@/components/Layout'
 import { theme } from '@/styles/theme'
+import { Layout } from '@/components/Layout'
+import { Navbar } from '@/components/Navbar'
+import styled from '@emotion/styled'
+
+const AppWrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.gray[100]};
+  display: flex;
+  justify-content: center;
+`
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalReset} />
-      <Layout>
-        <h1>홈페이지 입니다.</h1>
-      </Layout>
+      <AppWrapper>
+        <Layout>
+          <Navbar />
+          <main>
+            <h1>컨텐츠 영역</h1>
+          </main>
+        </Layout>
+      </AppWrapper>
     </ThemeProvider>
   )
 }
-export default App 
+
+export default App
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,28 @@
 import styled from '@emotion/styled'
 import type { ReactNode } from 'react'
 
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  
+  & > .inner {
+    width: 100%;
+    max-width: 720px;
+    padding: 0 16px; 
+    box-sizing: border-box;
+  }
+`
+
 interface Props {
   children: ReactNode
 }
 
-const Wrapper = styled.div`
-  max-width: 720px;
-  margin: 0 auto;
-  padding: 0 16px;
-  width: 100%;
-  background-color: ${({ theme }) => theme.colors.background.default};
-`
-
 export const Layout = ({ children }: Props) => {
-  return <Wrapper>{children}</Wrapper>
+  return (
+  <Wrapper>
+    <div className="inner">{children}</div>
+  </Wrapper>
+  )
 }
