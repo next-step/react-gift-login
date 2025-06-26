83440e48dad8c1b795fc3a557f59a0e7974cd543
aaaaaattt <choigur03@gmail.com>
2025-06-26 10:25:36 +0900

    feat : 카테고리 섹션 구현

---
+++ src/components/Category.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import categoryData from "@/categoryData";
+import type { Theme } from "@emotion/react";
+import { css } from "@emotion/react";
+
+const Category = () => {
+  return (
+    <div css={(theme: Theme) => categoryStyle(theme)}>
+      {categoryData.map((category) => (
+        <div key={category.themeId} className="category-item">
+          <img
+            src={category.image}
+            alt={category.name}
+            className="category-image"
+            css={(theme) => imageStyle(theme)}
+          />
+          <h3 className="category-name">{category.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const categoryStyle = (theme: Theme) => css`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr); /* 한 줄에 3개 */
+  gap: 16px; /* 아이템 간 간격 */
+  font-size: ${theme.typography.label2Bold.size};
+  font-weight: ${theme.typography.label2Bold.weight};
+  line-height: ${theme.typography.label2Bold.lineHeight};
+  text-align: center;
+  padding: ${theme.spacing.spacing3} ${theme.spacing.spacing4};
+  background: ${theme.colors.semantic.background.default};
+  border-bottom: 1px solid ${theme.colors.semantic.border.default};
+`;
+
+const imageStyle = (theme: Theme) => css`
+  width: 100%;
+  max-width: 120px;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+  display: block;
+  border-radius: 8px;
+`;
+
+export default Category;
