9307b55af02c93d0161c9e014d546ab307ed162b
MINKI SUNG <162106492+wannagola@users.noreply.github.com>
2025-06-27 02:28:35 +0900

    feat : Emotion style setup & reset + Pretendard font

--- README.md
+++ README.md
@@ -10,8 +10,8 @@
 
 # Prettier가 무엇인지 찾아보고 추가해주세요. : CLEAR
 
-# emotion 스타일 라이브러리를 추가하고, reset css를 적용해주세요.
+# emotion 스타일 라이브러리를 추가하고, reset css를 적용해주세요. : CLEAR
 
-# 기본 폰트로 Pretendard를 적용해주세요.
+# 기본 폰트로 Pretendard를 적용해주세요. : CLEAR
 
-# 기능 단위로 나누어 커밋을 해주세요.
+# 기능 단위로 나누어 커밋을 해주세요. : CLEAR
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@/index.css'
-import App from '@/App.tsx'
+/** @jsxImportSource @emotion/react */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Global } from '@emotion/react';
+import reset from '@/styles/reset';
+import '@/index.css';
+import App from '@/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global styles={reset} />
     <App />
   </StrictMode>,
-)
+);
