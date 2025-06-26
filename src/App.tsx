673d2b37cc66ff9ab4b69cf4313ef36591d6afc2
kanghaeun <xgkg0330@jnu.ac.kr>
2025-06-26 16:25:03 +0900

    feat: 라우터 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Global } from '@emotion/react';
+import { Router } from '@/router/Router.jsx';
 import reset from './styles/reset';
 
 function App() {
   return (
     <>
       <Global styles={reset} />
+      <Router />
     </>
   );
 }
---
+++ src/pages/GiftHome.tsx
@@ -0,0 +1,5 @@
+const GiftHome = () => {
+  return <></>;
+};
+
+export default GiftHome;
