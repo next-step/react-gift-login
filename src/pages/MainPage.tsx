0403678a185714c9690c931555ab00c216ef10f7
Injun Jeong <jijun897@naver.com>
2025-06-26 11:35:01 +0900

    feat: 카테고리 섹션 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,11 @@
+import PresentCategory from "@/components/PresentCategory";
 import TheHeader from "@/components/TheHeader";
 
 const MainPage = () => {
   return (
     <>
       <TheHeader />
-      카카오 테크 캠퍼스 2단계 미션 1
+      <PresentCategory />
     </>
   );
 };
