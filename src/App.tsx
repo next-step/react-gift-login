fcff02ee5ea00ff72f4c14659517d8b7ff5ace0b
ganimjeong <86656605+ganimjeong@users.noreply.github.com>
2025-06-27 01:12:08 +0900

    전남대 FE 정아진 2단계 - 선물하기 메인페이지 UI 구현하기 (#125)
    * chore: 불필요한 파일 정리 및 reset.css 적용
    
    * chore: alias 설정 추가
    
    * chore: Prettier 설정 및 적용
    
    * feat: emotion 스타일링 환경 구축
    
    * style: Pretendard 폰트 기본 적용
    
    * chore: GlobalStyle에 reset.css 적용
    
    * chore: TypeScript 설치
    
    * refactor: alias 설정 적용 및 경로 수정
    
    * build: 프로젝트 빌드 결과물(dist) 추가
    
    * docs: 2단계 구현 명세 README에 정리
    
    * docs: 2단계 구현 명세 README에 정리
    
    * chore: emotion 컬러/타이포그래피/간격 토큰 설정
    
    * Mobile First Design 기반의 Layout컴포넌트 추가 및 설정
    
    * feat: 네비게이션 바 UI 컴포넌트 추가
    
    * feat: 카테고리 섹션 UI 구현
    
    * feat: 선물할 친구 선택 섹션 구현
    
    * feat: 실시간 선물랭킹 섹션 UI 구현
    
    ---------
    
    Co-authored-by: Ajin <ajin@Ajinui-noteubug.local>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import GlobalStyle from '@/GlobalStyle';
+
+import GlobalStyle from '@/styles/GlobalStyle';
+import NavigationBar from '@/components/NavigationBar/NavigationBar';
+import CategorySection from '@/components/Category/CategorySection';
+import SectionTitle from '@/components/SectionTitle';
+import GiftReceiverSelect from './components/GiftReceiverSelect/GiftReceiverSelect';
+import GiftRankingSection from './components/GiftRanking/GiftRankingSection';
+import Layout from './components/Layout';
+
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <div>스타일 확인용!</div>
+      <Layout>
+        <GlobalStyle />
+        <NavigationBar />
+        <GiftReceiverSelect />
+        <SectionTitle title="선물 테마" />
+        <CategorySection />
+        <SectionTitle title="실시간 급상승 선물랭킹" />
+        <GiftRankingSection />
+      </Layout>
+
     </>
   );
 }
