cc928435936a55ee9b38c27534ff41ff4e4932d3
Changmok LEE <cmlee5075@gmail.com>
2025-06-26 13:25:41 +0900

    chore: 불필요한 주석 제거

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -2,6 +2,5 @@ import '@emotion/react';
 import type { AppTheme } from '@/styles/theme';
 
 declare module '@emotion/react' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Theme extends AppTheme {}
 }
