19097eea248c07e4bc5bdaa58f4c7b2fae52b72c
meem3443 <jinsr0235@gmail.com>
2025-06-26 12:35:28 +0900

    feat: Mobile First Design Layout Set

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          md: "780px",
+          lg: "780px",
+          xl: "780px",
+          "2xl": "780px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
