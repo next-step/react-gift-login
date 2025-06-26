ab106bb5e9123f5057449692cb4c2b74b6d51077
cktjgus <cktjgus04@naver.com>
2025-06-26 16:40:23 +0900

    Category section 수정

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -3,34 +3,46 @@ import { categories } from '@/data/categories'
 
 export default function Category() {
   return (
-    <Section>
-      <Title>선물 테마</Title>
-      <Grid>
-        {categories.map((item) => (
-          <CategoryItem key={item.themeId}>
-            <Image src={item.image} alt={item.name} />
-            <Label>{item.name}</Label>
-          </CategoryItem>
-        ))}
-      </Grid>
-    </Section>
+    <>
+      <Block/>
+      <Section>
+        <Title>선물 테마</Title>
+        <Grid>
+          {categories.map((item) => (
+            <CategoryItem key={item.themeId}>
+              <Image src={item.image} alt={item.name} />
+              <Label>{item.name}</Label>
+            </CategoryItem>
+          ))}
+        </Grid>
+      </Section>
+      <Block/>
+    </>
   )
 }
 
+const Block = styled.section`
+  width: 100%;
+  height: 24px;
+  background-color: transparent;
+`
+
 const Section = styled.section`
-  padding: 24px 0;
+  padding: 8px 0;
 `
 
 const Title = styled.h2`
-  font-size: 16px;
-  font-weight: bold;
-  margin-bottom: 16px;
+  ${({ theme }) => theme.typography.title1Bold};
+  padding: 0px 8px 20px;
+  margin: 0px;
+  width: 100px;
+  text-align: left;
 `
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr); // 5개씩 한 줄
-  gap: 16px 8px;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 20px 4px;
 
   @media (max-width: 480px) {
     grid-template-columns: repeat(4, 1fr); // 모바일은 4개씩
@@ -41,16 +53,22 @@ const CategoryItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+  gap: 4px;
+  cursor: pointer;
 `
 
 const Image = styled.img`
-  width: 56px;
-  height: 56px;
+  max-width: 50px;
+  max-height: 50px;
+  width: 100%;
+  border-radius: 18px;
   object-fit: cover;
+  overflow: hidden;
 `
 
 const Label = styled.span`
-  font-size: 12px;
-  margin-top: 4px;
-  text-align: center;
+  ${({ theme }) => theme.typography.label2Regular};
+  margin: 0px;
+  text-align: left;
 `
