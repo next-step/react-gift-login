5d80b2dd76ab67d5f23000fb5967b9d6d380bf1f
Young Seo <givpro22@daum.net>
2025-06-26 16:17:58 +0900

    feat: 카테고리 섹션 컴포넌트 구현 및 테마 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header';
+import CategorySection from '@/components/CategorySection';
 
 function App() {
   return (
     <div>
       <Header />
+      <CategorySection />
     </div>
   );
 }
