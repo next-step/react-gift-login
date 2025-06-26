10509ec2d15f0bc839808acb2088d946d3efdd18
jiwonWP <bread0264@gmail.com>
2025-06-26 13:46:55 +0900

    feat: 기타 섹션 (선물한 친구 선택, 카테캠 화이팅) UI 구현

--- src/pages/GiftMain.tsx
+++ src/pages/GiftMain.tsx
@@ -1,14 +1,17 @@
 import { PageContainer } from '@/components/layout/PageContainer';
 import { Navigation } from '@/components/header/Navigation';
 import { CategorySection } from '@/components/category/CategorySection';
+import { FriendBanner } from '@/components/banner/FriendBanner';
+import { MessageBanner } from '@/components/banner/MessageBanner';
 
 
 const GiftMain= () =>{
     return (
         <PageContainer>
             <Navigation />
+            <FriendBanner />
             <CategorySection />
-
+            <MessageBanner />
             <h1>카카오 선물하기 메인 페이지</h1>
         </PageContainer>
     );
