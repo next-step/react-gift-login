748de97bacf6c614bf2b8a1bfed2fc03f2769561
hyemin <hyemin6647@naver.com>
2025-06-26 11:57:22 +0900

    style: 스타일링 초기화

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,10 +1,42 @@
-import { Global, css } from '@emotion/react'
+import { Global, css } from "@emotion/react";
 
 const GlobalStyles = () => {
   return (
     <Global
       styles={css`
         @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css");
+        /* 박스크기계산 방식 통일: padding 과 border 포함해서 계산 */
+
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+        /* 마진, 패딩 제거 */
+        body,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        ul,
+        ol,
+        li,
+        figure,
+        blockquote,
+        dl,
+        dd {
+          margin: 0;
+          padding: 0;
+        }
+        /* 리스트 스타일 제거 */
+        ul,
+        ol {
+          list-style: none;
+        }
+        /* 폰트 적용 */
         * {
           font-family:
             "Pretendard Variable",
@@ -24,8 +56,8 @@ const GlobalStyles = () => {
             sans-serif;
         }
       `}
-    /> 
+    />
   );
-}
+};
 
-export default GlobalStyles
+export default GlobalStyles;
