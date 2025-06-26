7cd8bc557007910339d58c128f5a43783ea89b93
aaaaaattt <choigur03@gmail.com>
2025-06-26 10:38:07 +0900

    feat : 기타 섹션 UI 구현 - 선물한 친구 선택 - 응원 문구

---
+++ src/components/SelectFriend.tsx
@@ -0,0 +1,58 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import type { Theme } from "@emotion/react";
+import { IoAdd } from "react-icons/io5"; // 추가된 아이콘
+
+const SelectFriend = () => {
+  return (
+    <div css={(theme) => containerStyle(theme)}>
+      <div css={(theme) => contentsStyle(theme)}>
+        <IoAdd css={(theme: Theme) => buttonStyle(theme)} />
+        <span css={(theme) => textStyle(theme)}>선물할 친구를 선택하세요</span>
+      </div>
+    </div>
+  );
+};
+
+const containerStyle = (theme: Theme) => css`
+  display: flex;
+  align-items: center;
+  justify-content: center; /* 가로 가운데 정렬 */
+  gap: 8px;
+  width: 100%;
+  min-height: 80px;
+  background-color: ${theme.colors.gray.gray100};
+`;
+const buttonStyle = (theme: Theme) => css`
+  color: black;
+  background-color: ${theme.colors.semantic.kakaoYellow};
+  font-size: 32px;
+  margin-right: 8px;
+  border-radius: 10px;
+  margin-left: 0px;
+  width: 36px;
+  height: 36px;
+`;
+
+const contentsStyle = (theme: Theme) => css`
+  display: flex;
+  align-items: center;
+  width: 95%;
+  height: 50px;
+  background-color: ${theme.colors.semantic.background.default};
+  color: ${theme.colors.semantic.text.default};
+  padding-left: 16px;
+  padding-right: 16px;
+  border: 1px none;
+  border-radius: 16px;
+`;
+
+const textStyle = (theme: Theme) => css`
+  font-size: ${theme.typography.subtitle1Bold.size};
+  font-weight: ${theme.typography.subtitle1Bold.weight};
+  line-height: ${theme.typography.subtitle1Bold.lineHeight};
+  color: ${theme.colors.semantic.text.default};
+  border-radius: 16px;
+`;
+
+export default SelectFriend;
