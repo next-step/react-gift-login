e5f56c36ae3b4b0163853f1b3b53f01c36896201
kimgho <kgh67811@knu.ac.kr>
2025-06-27 01:46:46 +0900

    feat(component): SelectFriendSection 컴포넌트 추가

---
+++ src/components/SelectFriendSection.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import { Plus } from "lucide-react";
+
+const SectionWrapper = styled.section(props => ({
+  display: "flex",
+  padding: `${props.theme.spacing4} ${props.theme.spacing3}}`,
+  backgroundColor: props.theme.color.gray[200],
+}));
+
+const SectionContainer = styled.div(props => ({
+  display: "flex",
+  alignItems: "center",
+  gap: `${props.theme.spacing3}`,
+  width: "100%",
+  height: "74px",
+  padding: `${props.theme.spacing4}`,
+  backgroundColor: "white",
+  borderRadius: "18px",
+}));
+
+const IconCircle = styled.div(props => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "42px",
+  height: "42px",
+  backgroundColor: `${props.theme.color.yellow[600]}`,
+  borderRadius: "16px",
+}));
+
+const SectionDescription = styled.p(props => ({
+  fontSize: `${props.theme.typography.body1Bold.fontSize}px`,
+  lineHeight: `${props.theme.typography.body1Bold.lineHeight}`,
+  fontWeight: `${props.theme.typography.body1Bold.fontWeight}`,
+  color: props.theme.color.gray[900],
+}));
+
+const SelectFriendSection = () => {
+  return (
+    <SectionWrapper>
+      <SectionContainer>
+        <IconCircle>
+          <Plus />
+        </IconCircle>
+        <SectionDescription>선물할 친구를 선택해 주세요.</SectionDescription>
+      </SectionContainer>
+    </SectionWrapper>
+  );
+};
+
+export default SelectFriendSection;
