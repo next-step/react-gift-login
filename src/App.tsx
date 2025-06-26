717dc18b15e722cee55a37cd30eecd381e0544e9
SonJH7 <sjunh02@naver.com>
2025-06-26 18:03:46 +0900

    feature(Category) : 카테코리 섹션 구현
    - CategoryCard 컴포넌트 추가 이미지와 이름 렌더링하도록 구현
    - CategorySection을 만들어 Mock 데이터 기반으로 선물 테마 목록을 5열 그리도 형태로 배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Layout from '@/Layout'
 import logo from '@/assets/logo.png'
 import FriendSelector from '@/components/FriendSelector'
+import CategorySection from '@/components/CategorySection'
+
 
 const friends = ['라이언', '무지', '콘']
 function App() {
   return (
     <Layout logoSrc={logo}>
       <FriendSelector friends={friends} />
+      <CategorySection />
     </Layout>
   )
 }
---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,38 @@
+﻿import styled from '@emotion/styled'
+
+interface CategoryCardProps {
+  name: string
+  image: string
+}
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`
+
+const Image = styled.img`
+  width: 72px;
+  height: 72px;
+  border-radius: 12px;
+  object-fit: cover;
+`
+
+const Name = styled.p`
+  font-size: 0.875rem;
+  font-weight: 400;
+  line-height: 1.1875rem;
+  color: #2a3038;
+`
+
+const CategoryCard = ({ name, image }: CategoryCardProps) => {
+  return (
+    <Card>
+      <Image src={image} alt={name} />
+      <Name>{name}</Name>
+    </Card>
+  )
+}
+
+export default CategoryCard
