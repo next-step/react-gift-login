1555bfd8aaeb77ee18e0dee2714d1c5706e9e139
toothlessdev <toothless042@gmail.com>
2025-06-26 17:47:35 +0900

    feat: 테마 관련 색상, 간격 및 타이포그래피 토큰 추가

---
+++ src/@types/emotion.d.ts
@@ -0,0 +1,12 @@
+import { colorToken } from "@/app/theme/colors";
+import { spacingToken } from "@/app/theme/spacing";
+import { typographyToken } from "@/app/theme/typography";
+import "@emotion/react";
+
+declare module "@emotion/react" {
+    export interface Theme {
+        colors: typeof colorToken;
+        typography: typeof typographyToken;
+        spacing: typeof spacingToken;
+    }
+}
