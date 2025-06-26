d0effd881fb74ba61f0586521b92a8bb2fdb9034
Chahyun Lee <163325051+chahyunlee@users.noreply.github.com>
2025-06-26 14:55:19 +0900

    feat: 카테고리 mockdata 추가 및 카테고리 섹션 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Global } from '@emotion/react';
 import reset from "@/styles/reset";
 import Layout from "@/layout/Layout"
 import NavigationBar from "@/components/NavigationBar"
+import CategorySection from "@/components/CategorySection"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Global styles={reset}/>
       <Layout>
         <NavigationBar/>
+        <CategorySection/>
       </Layout>
     </>
   );
---
+++ src/components/CategorySection.tsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled'
+import { categoryList } from '@/mockdata/categoryList'
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 0;
+`
+const Title = styled.h1`
+  ${({ theme }) => `
+    color: ${theme.colors.textDefault};
+    font-size: ${theme.typography.title1Bold.fontSize};
+    font-weight: ${theme.typography.title1Bold.fontWeight};
+    line-height: ${theme.typography.title1Bold.lineHeight};
+  `}
+  align-self: flex-start;
+  margin-left: 20px;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 10px 40px;
+`
+const CategoryItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`
+const CategoryImage = styled.img`
+  width: 60%;
+`
+const CategoryName = styled.span`
+
+`
+
+const CategorySection = () => {
+  return (
+    <Container>
+      <Title>선물 테마</Title>
+      <Grid>
+        {categoryList.map((category)=>(
+          <CategoryItem key = {category.themeId}>
+            <CategoryImage src = {category.image}/>
+            <CategoryName>{category.name}</CategoryName>
+          </CategoryItem>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
+
+export default CategorySection;
