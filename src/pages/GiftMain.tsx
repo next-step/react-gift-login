db2d588ff2d171769944507333c97455403e54de
jiwonWP <bread0264@gmail.com>
2025-06-26 11:47:59 +0900

    feat: 상단 네비게이션 바 구현

--- src/pages/GiftMain.tsx
+++ src/pages/GiftMain.tsx
@@ -1,9 +1,11 @@
 import { PageContainer } from '@/components/layout/PageContainer';
+import { Navigation } from '@/components/header/Navigation';
 
 
 const GiftMain= () =>{
     return (
         <PageContainer>
+            <Navigation />
             <h1>카카오 선물하기 메인 페이지</h1>
         </PageContainer>
     );
