d15100dce04c390cf948bd567cd55159210af8b4
aaaaaattt <choigur03@gmail.com>
2025-06-26 09:29:27 +0900

    feat : globalStyle 추가

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { Global, css } from "@emotion/react";
+
+export const GlobalStyle = () => (
+  <Global
+    styles={css`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
