d5d70e9638d92afbb1c08e870dd4bda26c39f59b
leehuiseok <leehuiseok020412@gmail.com>
2025-06-26 13:14:01 +0900

    feat: 카테고리 섹션구현

--- src/pages/GiftPage.tsx
+++ src/pages/GiftPage.tsx
@@ -1,5 +1,8 @@
-import styled from '@emotion/styled/macro';
+import styled from '@emotion/styled';
 import { colors } from '@/styles/tokens';
+import { Header } from '@/components/Header/Header';
+import { CategoryGrid } from '@/components/CategoryGrid/CategoryGrid';
+import { categories } from '@/mock/mockData';
 
 const AppContainer = styled.div`
   max-width: 720px;
@@ -9,5 +12,18 @@ const AppContainer = styled.div`
 `;
 
 export const GiftPage = () => {
-  return <AppContainer></AppContainer>;
+  return (
+    <AppContainer>
+      <Header
+        title="선물하기"
+        onBackClick={() => console.log('뒤로가기 버튼 누름')}
+        onProfileClick={() => console.log('프로필 버튼 누름')}
+      />
+
+      <CategoryGrid
+        categories={categories}
+        onCategoryClick={(category) => console.log('카테고리 클릭', category)}
+      />
+    </AppContainer>
+  );
 };
