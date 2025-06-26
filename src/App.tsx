afdff0942d43ba1b1d55455243fd327bf0700239
jmj <waysoleward01@naver.com>
2025-06-26 15:14:25 +0900

    feat: 네비게이션 바 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Global } from "@emotion/react";
 import { reset } from "./styles/reset";
 import { globalStyle } from "./styles/globalStyle";
+import { NavigationBar } from "./components/NavigationBar";
 
 function App() {
   return (
     <>
       <Global styles={reset} />
       <Global styles={(theme) => globalStyle(theme)} />
+      <NavigationBar />
       <div>홈 화면입니다</div>
     </>
   );
