259b3e16beec79e203b97116bd4b0892ac0b5572
kanghaeun <xgkg0330@jnu.ac.kr>
2025-06-26 18:45:24 +0900

    feat: GiftHomePage에 컴포넌트 적용

--- src/pages/GiftHome.tsx
+++ src/pages/GiftHome.tsx
@@ -1,5 +1,29 @@
+import styled from '@emotion/styled';
+import NavigationBar from '@/components/NavigationBar';
+import SelectReceiverNotice from '@/components/SelectReceiverNotice';
+import GiftCategoryList from '@/components/GiftCategoryList';
+import GiftChart from '@/components/GiftChart';
 const GiftHome = () => {
-  return <></>;
+  return (
+    <Layout>
+      <Content>
+        <NavigationBar />
+        <SelectReceiverNotice />
+        <GiftCategoryList />
+        <GiftChart />
+      </Content>
+    </Layout>
+  );
 };
 
 export default GiftHome;
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 720px;
+`;
