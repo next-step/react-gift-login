1d0dafa9350fae48840fda46df9eeff8ed35f32e
SonJH7 <sjunh02@naver.com>
2025-06-26 18:11:33 +0900

    feat(CampusBanner): 카테캠 화이팅 배너 완성
    - 3기 응원 문구 추가
    - 여기서부터 잊고 있던 color, typography 토큰 적용
    - space도 적용할 예정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Layout from '@/Layout'
 import logo from '@/assets/logo.png'
 import FriendSelector from '@/components/FriendSelector'
 import CategorySection from '@/components/CategorySection'
-
+import CampusBanner from '@/components/CampusBanner'
 
 const friends = ['라이언', '무지', '콘']
 function App() {
@@ -10,6 +10,7 @@ function App() {
     <Layout logoSrc={logo}>
       <FriendSelector friends={friends} />
       <CategorySection />
+      <CampusBanner />
     </Layout>
   )
 }
