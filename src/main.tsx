7b9730840c83702766734ce7305657fd7756cd1b
thgus0 <sohyun10521052@gmail.com>
2025-06-26 14:53:50 +0900

    chore: emotion에 Color, Typography, Spacing 토큰 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@/index.css'
 import App from '@/App.tsx'
-import { GlobalStyles } from '@/GlobalStyles'
+import { GlobalStyles } from '@/styles/GlobalStyles'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from '@/styles/theme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyles />
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
