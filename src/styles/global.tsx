1abd40a56fab6d83fd51eea428b5e34ac6ae4680
s202055564 <hcan1223@pusan.ac.kr>
2025-06-26 15:43:13 +0900

    max width 및 global style 설정
    global.tsx 코드 수정

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,13 +1,23 @@
-/** @jsxImportSource @emotion/react */
+
 import { Global, css } from '@emotion/react'
 import reset from './reset'
 
-const GlobalStyle = () => (
-    <Global
-        styles={css`
-      ${reset}
-    `}
-    />
-)
+export const globalStyles = css`
+  ${reset};
 
-export default GlobalStyle
+  body {
+    background: #fff;
+    font-family: sans-serif;
+    color: #222;
+  }
+
+  #root {
+    max-width: 720px;
+    margin: 0 auto;
+    padding: 1rem;
+  }
+`;
+
+const GlobalStyle = () => <Global styles={globalStyles} />;
+
+export default GlobalStyle;
