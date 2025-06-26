ce406f01297cb5a08c377b88d171175b839c587d
HanseKim <kimbird2002@naver.com>
2025-06-26 16:36:07 +0900

    feat: 선물 종류 박스 UI 완성

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,5 +1,20 @@
-function Category() {
-  return <li>카테고리</li>;
+import { CategoryContainerStyle } from '@/styles/CategoryStyle.styles';
+
+function Category({
+  category,
+  onClick,
+  selectCategory,
+}: {
+  category: string;
+  onClick?: () => void;
+  selectCategory?: string;
+}) {
+  const selected = category === selectCategory;
+  return (
+    <CategoryContainerStyle onClick={onClick} selected={selected}>
+      {category}
+    </CategoryContainerStyle>
+  );
 }
 
 export default Category;
--- src/components/Container/CategoryContainer.tsx
+++ src/components/Container/CategoryContainer.tsx
@@ -1,11 +1,30 @@
 import Category from '@/components/Category';
+import { CategoryContainerStyle } from '@/styles/CategoryContainer.styles';
+import { useState } from 'react';
 
 function CategoryContainer() {
+  const [selectCategory, setSelectCategory] = useState<string>('받고 싶어한');
+  function handleCategoryClick(category: string) {
+    setSelectCategory(category);
+  }
   return (
-    <ul>
-      * 카테고리 분류
-      <Category />
-    </ul>
+    <CategoryContainerStyle>
+      <Category
+        onClick={() => handleCategoryClick('받고 싶어한')}
+        category="받고 싶어한"
+        selectCategory={selectCategory}
+      />
+      <Category
+        onClick={() => handleCategoryClick('많이 선물한')}
+        category="많이 선물한"
+        selectCategory={selectCategory}
+      />
+      <Category
+        onClick={() => handleCategoryClick('위시로 받은')}
+        category="위시로 받은"
+        selectCategory={selectCategory}
+      />
+    </CategoryContainerStyle>
   );
 }
 
