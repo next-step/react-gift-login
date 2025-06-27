01ffac38bdeb88bf910a65294665c8408c6744ad
aaaaaattt <choigur03@gmail.com>
2025-06-28 00:15:32 +0900

    chore : 아이콘 크기 변경과 배경색 추가

--- src/components/GiftRankingHeader.tsx
+++ src/components/GiftRankingHeader.tsx
@@ -5,18 +5,19 @@ import { IoAppsOutline, IoWomanOutline, IoManOutline } from "react-icons/io5";
 import { useTheme } from "@emotion/react";
 
 const GiftRankingHeader = () => {
+  const theme = useTheme();
   return (
     <>
       <div css={textStyle(theme)}>실시간 급상승 선물랭킹</div>
       <div css={containerStyle}>
-        <IoAppsOutline css={iconStyle} />
+        <IoAppsOutline css={iconStyle(theme)} />
         <IoWomanOutline css={iconStyle} />
         <IoManOutline css={iconStyle} />
       </div>
-      <div css={tabContainerStyle}>
-        <div css={tabItemStyle}>받고 싶어한</div>
-        <div css={tabItemStyle}>많이 선물한</div>
-        <div css={tabItemStyle}>위시로 받은</div>
+      <div css={tabContainerStyle(theme)}>
+        <div css={tabItemStyle(theme)}>받고 싶어한</div>
+        <div css={tabItemStyle(theme)}>많이 선물한</div>
+        <div css={tabItemStyle(theme)}>위시로 받은</div>
       </div>
     </>
   );
@@ -37,28 +38,33 @@ const textStyle = (theme: Theme) => css`
 const containerStyle = css`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   flex-direction: row;
-  gap: 8px;
+  gap: 200px;
   width: 100%;
   min-height: 80px;
   border-radius: 16px;
 `;
 
-const iconStyle = css`
+const iconStyle = (theme: Theme) => css`
   font-size: 24px;
   cursor: pointer;
+  border-radius: 20%;
+  width: 36px;
+  height: 36px;
 `;
 
-const tabContainerStyle = css`
+const tabContainerStyle = (theme: Theme) => css`
   display: flex;
   justify-content: space-between;
   border-radius: 8px;
   padding: 10px;
   width: 100%;
+  border: 1px solid ${theme.colors.semantic.border.default};
+  background-color: ${theme.colors.blue.blue100};
 `;
 
-const tabItemStyle = css`
+const tabItemStyle = (theme: Theme) => css`
   flex: 1;
   padding: 12px 16px;
   text-align: center;
