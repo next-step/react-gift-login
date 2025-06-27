3ee22afde7571440fb0000fd6ac4d0448cfa3b99
younghyunChoi <lewis177777@gmail.com>
2025-06-27 16:54:01 +0900

    전남대 FE 최영현 - 2단계 선물하기 메인 페이지 UI 구현하기 (#97)
    * 데이터,초기설정
    
    * emotion theme
    
    * 카테고리 구현
    
    * 실시간 급상승 선물랭킹
    
    * 네비게이션,선물할 친구,카테캠 화이팅
    
    * loadbutton삭제
    
    ---------
    
    Co-authored-by: thighnurger <lewis177777gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@', replacement: resolve(__dirname, 'src') }, // 절대 경로로 설정
-      { find: '@styles', replacement: resolve(__dirname, 'src/styles') }, // 절대 경로로 설정
-    ],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
-  plugins: [react(), tsconfigPaths()],
-});
+})
