4f806caa0a6526e65065e0a7189021be94752dbc
JinVibe <loonaticvibe2.11@gmail.com>
2025-06-27 23:10:54 +0900

    feat: NavBar 구현

--- src/Components/layout/Layout.tsx
+++ src/Components/layout/Layout.tsx
@@ -1,12 +1,18 @@
 import styled from '@emotion/styled'
 import type { ReactNode } from 'react'
+import Header from './Header'
 
 interface LayoutProps {
   children: ReactNode
 }
 
 export const Layout = ({ children }: LayoutProps) => {
-  return <Wrapper>{children}</Wrapper>
+  return (
+    <Wrapper>
+      <Header />
+      {children}
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
