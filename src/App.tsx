72c52ca80153a69e96eec285ebd2265fbbf722e5
Jaeho <sjaeho348@gmail.com>
2025-06-28 00:21:00 +0900

    chore : themeprovider를 main.tsx로 이동, babel플러그인 설정추가(최적화)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { ThemeProvider } from '@emotion/react';
-import { theme } from './styles/theme';
 import Navigation from './components/Navigation';
 import FriendSelector from './components/FriendSelector';
 import CategorySection from './components/CategorySection';
@@ -8,13 +6,13 @@ import RankingSection from './components/RankingSection';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Navigation />
       <FriendSelector />
       <CategorySection />
       <MessageSection />
       <RankingSection />
-    </ThemeProvider>
+    </>
   )
 }
 
