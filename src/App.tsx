1da349e9c1016c22ec0ce0ff8454cf57c9c08727
suhyeon357 <ksuhy0507@gmail.com>
2025-06-27 12:42:49 +0900

    강원대 FE 김수현 2단계 - 선물하기 메인 페이지 UI 구현하기 (#113)
    * 불필요한 코드 정리
    
    * 과제 진행 요구사항
    
    * 요구사항 수정
    
    * 과제 요구사항
    
    * 과제 요구사항
    
    * Prettier 적용
    
    * Prettier 적용
    
    * Pretendard 적용
    
    * Pretendard 체크
    
    * emotion 추가, reset css 적용
    
    * alias 설정
    
    * step2 시작
    
    * emotion 스타일 colors, typography 토큰 추가
    
    * emotion 스타일 color, typography 토큰 추가 표시
    
    * 상단 네비게이션 바 구현
    
    * 카테고리 섹션 구현
    
    * 카테고리 섹션 구현 체크
    
    * 기타 섹션 구현
    
    * 기타 섹션 구현 체크
    
    * 실시간 급상승 선물 랭킹 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import { Global } from '@emotion/react';
-import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { Global, ThemeProvider, css } from '@emotion/react';
 import { reset } from './styles/reset';
+import { theme } from './styles/theme';
+import { FriendSelect } from './components/FriendSelect';
+import { Header } from './components/Header';
+import { GiftThemeGrid } from './components/GiftThemeGrid';
+import { YellowBanner } from './components/YellowBanner';
+import { RankingGrid } from './components/RankingGrid';
+import GiftRankingFilter from './components/GiftRankingFilter';
+
+const containerStyle = css`
+  max-width: 720px;
+  margin: 0 auto;
+  padding: 16px;
+  background-color: ${theme.colors.backgroundDefault};
+`;
 
 const App = () => {
   return (
-    <>
-      <Global styles={reset}></Global>
-    </>
+    <ThemeProvider theme={theme}>
+      <Global styles={reset} />
+      <div css={containerStyle}>
+        <Header />
+        <FriendSelect />
+        <GiftThemeGrid />
+        <YellowBanner />
+        <GiftRankingFilter />
+        <RankingGrid />
+      </div>
+    </ThemeProvider>
   );
 };
 
