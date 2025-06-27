49bc25e869c42314c08fbbfb7bc6d8cfb6b5be40
kimgho <kgh67811@knu.ac.kr>
2025-06-27 16:15:57 +0900

    feat(router): react-rotuer-dom으로 라우팅 구현

---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import MainPage from "@/pages/MainPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
