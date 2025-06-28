f7bda05d29a784d4beaa5bcd7056312d0527ceb8
Taewoong Heo <chamjoeun0111@gmail.com>
2025-06-28 19:55:10 +0900

    refactor: 로그인 메시지 상수 동결

--- src/components/Login/constants/labels.ts
+++ src/components/Login/constants/labels.ts
@@ -1,5 +1,7 @@
-export const LOGIN_LABELS = {
+import { deepFreeze } from '@/utils/deepFreeze';
+
+export const LOGIN_LABELS = deepFreeze({
   EMAIL_PLACEHOLDER: '이메일',
   PASSWORD_PLACEHOLDER: '비밀번호',
   LOGIN_BUTTON: '로그인',
-} as const;
+} as const);
