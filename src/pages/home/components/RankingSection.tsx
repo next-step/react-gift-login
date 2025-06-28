5084049016e5a222b3d488c88968fb5c73a0e9bf
jmj <waysoleward01@naver.com>
2025-06-29 02:12:45 +0900

    feat: 새로고침 시에도 필터 유지

--- src/pages/home/components/RankingSection.tsx
+++ src/pages/home/components/RankingSection.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RankingFilter } from "./RankingFilter";
 import { RankingTab } from "./RankingTab";
 import { RankingGrid } from "./RankingGrid";
@@ -9,9 +9,25 @@ import {
   type TabType,
 } from "../../../constants/ranking";
 
+const GENDER_KEY = "ranking_gender";
+const TAB_KEY = "ranking_tab";
+
 export const RankingSection = () => {
-  const [gender, setGender] = useState<GenderType>("ALL");
-  const [tab, setTab] = useState<TabType>(RANKING_TABS[0]);
+  const [gender, setGender] = useState<GenderType>(() => {
+    return (localStorage.getItem(GENDER_KEY) as GenderType) || "ALL";
+  });
+
+  const [tab, setTab] = useState<TabType>(() => {
+    return (localStorage.getItem(TAB_KEY) as TabType) || RANKING_TABS[0];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(GENDER_KEY, gender);
+  }, [gender]);
+
+  useEffect(() => {
+    localStorage.setItem(TAB_KEY, tab);
+  }, [tab]);
 
   return (
     <section css={sectionStyle}>
