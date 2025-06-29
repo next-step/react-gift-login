33572b126268cebd444daee67206f19d3a7c9e6e
cheog0 <https://github.com/cheog0>
2025-06-30 01:01:51 +0900

    refactor: ThemeItem → GiftThemeItem 네이밍 변경

--- src/components/GiftCategoryGrid.tsx
+++ src/components/GiftCategoryGrid.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { theme } from '@/styles/theme';
 import type { GiftTheme } from '@/types';
-import { ThemeItem } from './ThemeItem';
+import { GiftThemeItem } from './GiftThemeItem';
 
 interface GiftCategoryGridProps {
   themes: GiftTheme[];
@@ -46,7 +46,7 @@ export function GiftCategoryGrid({
       <SectionTitle>선물 테마</SectionTitle>
       <Grid>
         {themes.map(theme => (
-          <ThemeItem
+          <GiftThemeItem
             key={theme.themeId}
             theme={theme}
             onClick={handleThemeClick}
