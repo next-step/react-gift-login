02ef1989e2caa976fbae68bbdc5d3a133bb5558b
Jaeho <sjaeho348@gmail.com>
2025-06-29 14:52:03 +0900

    feat : NotFound페이지 구현

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 import Home from '../pages/Home/Home';
 import Login from '../pages/Login/Login';
+import NotFound from '../pages/NotFound/NotFound';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: 'login',
         element: <Login />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
       },
     ],
   },
