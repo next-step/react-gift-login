4d15c8464dfba7a86d1ae07b9c6693887a14b583
LeeHeeSung98989898 <goqpqp234@gmail.com>
2025-06-29 19:40:53 +0900

    refactor: step2 피드백 관련 개선(FriendSelectBanner로 컴포넌트명 변경, CategoryCard 컴포넌트 사용하도록 리팩토링, CategoryCard의 불필요 세미콜론 제거)

--- src/sections/CategorySection.tsx
+++ src/sections/CategorySection.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { categoryData } from "@/mocks/categoryData";
+import CategoryCard from "@/components/CategoryCard";
 
 const Section = styled.section`
   padding: ${({ theme }) => theme.spacing.spacing5} 0;
@@ -17,43 +18,16 @@ const Grid = styled.div`
   gap: ${({ theme }) => theme.spacing.spacing3};
 `;
 
-const CategoryItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const CategoryImage = styled.img`
-  width: 64px;
-  height: 64px;
-  border-radius: 50%;
-  margin-bottom: ${({ theme }) => theme.spacing.spacing1};
-`;
-
-const CategoryName = styled.span`
-  ${({ theme }) => theme.typography.label.label2Regular};
-  text-align: center;
-`;
-
-type Category = {
-    themeId: number;
-    name: string;
-    image: string;
-};
-
 export default function CategorySection() {
 
-    return (
-        <Section>
-            <SectionTitle>선물 테마</SectionTitle>
-            <Grid>
-                {categoryData.map((category: Category) => (
-                    <CategoryItem key={category.themeId}>
-                        <CategoryImage src={category.image} alt={category.name} />
-                        <CategoryName>{category.name}</CategoryName>
-                    </CategoryItem>
-                ))}
-            </Grid>
-        </Section>
-    );
+  return (
+    <Section>
+      <SectionTitle>선물 테마</SectionTitle>
+      <Grid>
+        {categoryData.map(({ themeId, name, image }) => (
+          <CategoryCard key={themeId} name={name} image={image} />
+        ))}
+      </Grid>
+    </Section>
+  );
 }
