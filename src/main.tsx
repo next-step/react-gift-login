0b2be24c7bd9b5d34e261de9454dc5ddd8a922e1
anseonghyeon <anseonghyeon@gmail.com>
2025-06-29 22:42:17 +0900

    feat(router): NotFound페이지를 구현하고 라우팅 처리를 해주었습니다

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 
 import App from './App.tsx'
 import Login from './Login.tsx'
+import NotFound from './NotFound.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<App />}></Route>
         <Route path='/Login' element={<Login />}></Route>
+        <Route path='/NotFound' element={<NotFound />}></Route>
+        <Route path='*' element={<Navigate to='/NotFound' replace />}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
