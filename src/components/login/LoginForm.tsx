2646435cef59eebc9f6a4e9ddc5a27a0d730e46e
kimgho <kgh67811@knu.ac.kr>
2025-06-30 00:24:51 +0900

    feat(input): Input 컴포넌트 분리
    - Input 컴포넌트를 공용으로 사용할 수 있도록 common에 배치

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/common";
 import { LoginButton } from "@/components/login/LoginButton";
 import styled from "@emotion/styled";
 
@@ -16,37 +17,15 @@ const LoginInputWrapper = styled.div(props => ({
   marginBottom: `${props.theme.spacing4}`,
 }));
 
-const LoginInput = styled.input(props => ({
-  width: "100%",
-  padding: `${props.theme.spacing2} 0`,
-  fontSize: `${props.theme.typography.label1Regular.fontSize}`,
-  lineHeight: `${props.theme.typography.label1Regular.lineHeight}`,
-  fontWeight: `${props.theme.typography.label1Regular.fontWeight}`,
-  border: "none",
-  borderBottom: "1px solid  rgb(220, 222, 227)",
-  outline: "none",
-  backgroundColor: "transparent",
-  transition: "border-bottom-color 0.2s ease",
-
-  "&::placeholder": {
-    color: "#999",
-    fontSize: "14px",
-  },
-
-  "&:focus": {
-    borderBottomColor: "#000",
-  },
-}));
-
 export const LoginForm = () => {
   return (
     <LoginFormContainer>
       <LoginInputWrapper>
-        <LoginInput placeholder="이메일" type="email" />
+        <Input placeholder="이메일" type="email" />
       </LoginInputWrapper>
 
       <LoginInputWrapper>
-        <LoginInput placeholder="비밀번호" type="password" />
+        <Input placeholder="비밀번호" type="password" />
       </LoginInputWrapper>
       <LoginButton />
     </LoginFormContainer>
