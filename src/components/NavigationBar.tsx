89cc4fba5302ca998a0209cb9dd36c8fbd368564
kanghaeun <xgkg0330@jnu.ac.kr>
2025-06-29 23:00:05 +0900

    feat: react-icons 설치 및 적용

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,19 +1,33 @@
 import styled from '@emotion/styled';
+import { IoIosArrowBack } from 'react-icons/io';
+import { LuUserRound } from 'react-icons/lu';
 
 const NavigationBar = () => {
-  return <Container>선물하기</Container>;
+  return (
+    <Layout>
+      <IoIosArrowBack size={25} />
+      <Container>선물하기</Container>
+      <LuUserRound size={25} />
+    </Layout>
+  );
 };
 
 export default NavigationBar;
+const Layout = styled.div`
+  box-sizing: border-box;
+  position: fixed;
+  padding: 0 8px 0 8px;
+  top: 0;
+  width: 100%;
+  max-width: 720px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 44px;
+  background-color: ${({ theme }) => theme.colors.gray00};
+`;
 
 const Container = styled.div`
   font-size: ${({ theme }) => theme.typography.title1Bold.fontSize};
   font-weight: ${({ theme }) => theme.typography.title2Bold.fontWeight};
-  position: fixed;
-  top: 0;
-  width: 720px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 44px;
 `;
