c70ea9df6c9650cd802b7d8e1b24bf4990f981b7
Jaeho <sjaeho348@gmail.com>
2025-06-29 12:44:28 +0900

    feat : router설치 및 로그인 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import Navigation from './components/Navigation/Navigation';
 import FriendSelector from './components/FriendSelector/FriendSelector';
 import CategorySection from './components/CategorySection/CategorySection';
 import MessageSection from './components/MessageSection/MessageSection';
 import RankingSection from './components/RankingSection/RankingSection';
+import Login from './pages/Login/Login';
+
+const HomePage = () => (
+  <>
+    <Navigation />
+    <FriendSelector />
+    <CategorySection />
+    <MessageSection />
+    <RankingSection />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+]);
 
 function App() {
-  return (
-    <>
-      <Navigation />
-      <FriendSelector />
-      <CategorySection />
-      <MessageSection />
-      <RankingSection />
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
