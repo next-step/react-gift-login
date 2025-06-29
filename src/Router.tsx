49af6414848cc3ef102e7ace42a47618c8436f84
toothlessdev <toothless042@gmail.com>
2025-06-30 00:37:37 +0900

    feature: AuthLayout 적용 및 NotFoundPage 라우팅 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
 import HomePage from "@/pages/HomePage";
+import NotFoundPage from "@/pages/NotFoundPage";
+import { AuthLayout } from "@/pages/auth/AuthLayout";
 import SignInPage from "@/pages/auth/SignInPage";
 
 import { RootLayout } from "@/widgets/layouts";
@@ -8,9 +10,10 @@ import { RootLayout } from "@/widgets/layouts";
 const router = createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/auth">
+        <Route path="/auth" element={<AuthLayout />}>
             <Route path="signin" element={<SignInPage />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
     </Route>,
 );
 
