ba00af24fa46a049023350ffdbffb87d05daa885
meem3443 <jinsr0235@gmail.com>
2025-06-29 21:40:44 +0900

    feat : tabs, sort constatnted

---
+++ src/constants/sortoptions.ts
@@ -0,0 +1,7 @@
+export const sortOptions: string[] = [
+  "받고 싶어한",
+  "많이 선물한",
+  "위시로 받은",
+];
+
+export default sortOptions;
---
+++ src/constants/tabs.ts
@@ -0,0 +1,3 @@
+export const tabs: string[] = ["전체", "여성이", "남성이", "청소년이"];
+
+export default tabs;
