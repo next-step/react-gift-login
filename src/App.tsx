525fc23b01a1c665712480c992530e9532716d3a
MINKI SUNG <162106492+wannagola@users.noreply.github.com>
2025-06-29 17:54:31 +0900

    feat: mobile first design : 기타 섹션(선물한 친구 선택, 배너 등) 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { css, useTheme } from '@emotion/react';
 import NavigationBar from '@/components/common/NavigationBar';
-import CategorySection from '@/components/home/CategorySection'; // ✅ 추가
+import CategorySection from '@/components/home/CategorySection';
+import FriendSelectSection from './components/home/FriendSelectSection';
+import BannerSection from './components/home/BannerSection';
 
 function App() {
   const theme = useTheme();
@@ -9,14 +11,23 @@ function App() {
   return (
     <div
       css={css`
-        max-width: 720px;
-        margin: 0 auto;
-        background-color: ${theme.backgroundColors.default};
+        background-color: ${theme.colors.gray100};
         min-height: 100vh;
       `}
     >
-      <NavigationBar />
-      <CategorySection /> 
+      <div
+        css={css`
+          max-width: 720px;
+          margin: 0 auto;
+          background-color: ${theme.colors.gray00};
+          min-height: 100vh;
+        `}
+      >
+        <NavigationBar />
+        <FriendSelectSection />
+        <CategorySection />
+        <BannerSection />
+      </div>
     </div>
   );
 }
