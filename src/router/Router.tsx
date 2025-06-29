205bc7eaba7100f441fafe04d9b0a44b4ae1994a
changhee-cho <changhee@kangwon.ac.kr>
2025-06-29 20:26:04 +0900

    feat: Not Found 페이지 구현

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '@src/pages/Home';
 import LoginPage from '@/pages/LoginPage';
+import NotFound from '@/pages/NotFound';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
