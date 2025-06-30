8904d4c12f3c790d15dd51fd09438d73d3d647a3
hojunlee <leehojun.full@gmail.com>
2025-06-30 15:31:27 +0900

    add - not found page add

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home';
+import Home from '@pages/Home';
 import Login from '@pages/Login';
+import NotFound from '@pages/NotFound';
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='login' element={<Login />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
