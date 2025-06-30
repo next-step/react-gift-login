f4551b5d66c0688187f27e2e2c81b5ec1f7d5b29
Changmok LEE <cmlee5075@gmail.com>
2025-06-30 14:28:57 +0900

    feat: /login 페이지 접속 시 로그인 화면으로 연결

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,10 +1,12 @@
 import GiftsPage from '@/page/GiftPage';
+import LoginPage from '@/page/LoginPage';
 import { Route, Routes } from 'react-router-dom';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<GiftsPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
