5edc18bdaefe2a239eef76a1bb82904cc8cad1f7
eodudrepublic <eodudrepublic@gmail.com>
2025-06-30 10:28:32 +0900

    feat(global): emotion에 Color, Typography 토큰을 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from '@/styles/GlobalStyle';
+import { theme }   from '@/styles/theme'; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyle />   {/* ← Reset + Pretendard 전역 적용 */}
-    <App />
+    <ThemeProvider theme={theme}> {/* 테마 컨텍스트 주입 */}
+      <GlobalStyle />             {/* Reset + Pretendard 전역 적용 */}
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
