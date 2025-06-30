1de1f4367a22dd9027b4a11f73d1a64a5c15062b
sinyeowon <ioohyou@knu.ac.kr>
2025-06-30 13:55:50 +0900

    refactor(step2): numeric seprator 일관화

--- src/data/products.ts
+++ src/data/products.ts
@@ -5,7 +5,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 29000,
+      basicPrice: 29_000,
       discountRate: 0,
       sellingPrice: 29_000,
     },
@@ -24,7 +24,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 28000,
+      basicPrice: 28_000,
       discountRate: 0,
       sellingPrice: 28_000,
     },
@@ -43,7 +43,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -62,7 +62,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -81,7 +81,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -100,7 +100,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -119,7 +119,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -138,7 +138,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -157,7 +157,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -176,7 +176,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -195,7 +195,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -214,7 +214,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -233,7 +233,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -252,7 +252,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -271,7 +271,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -290,7 +290,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -309,7 +309,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
@@ -328,7 +328,7 @@ export const products = [
     imageURL:
       'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
     price: {
-      basicPrice: 27000,
+      basicPrice: 27_000,
       discountRate: 0,
       sellingPrice: 27_000,
     },
