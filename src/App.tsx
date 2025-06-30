4b2af9673ec82f5c2ff59080bc94c11e5ac69fd7
JinVibe <loonaticvibe2.11@gmail.com>
2025-06-30 14:22:52 +0900

    feat: Not Found 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { theme as emotionTheme } from '@/styles/Theme'
 import GlobalStyle from '@/styles/GlobalStyle'
 import Home from '@/pages/Home'
 import Login from './pages/Login'
+import NotFound from './pages/NotFound'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
@@ -18,6 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </EmotionThemeProvider>
