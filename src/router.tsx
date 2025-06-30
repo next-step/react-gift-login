f3f11c48c4a07df3c9189a9acfed7821dddeae15
SonJH7 <sjunh02@naver.com>
2025-06-30 23:10:02 +0900

    feat(login): 로그인 페이지 추가 및 테마 스타일링 - /login 경로에서 해당 페이지를 렌더링하도록 라우터에 경로 등록

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 ﻿import { createBrowserRouter } from 'react-router-dom'
 import App from '@/App'
+import LoginPage from '@/pages/LoginPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
   },
 ])
 
