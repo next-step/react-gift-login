db11b63dcdeac270ca0d2197c47bdaae0ea88351
kanghaeun <145974230+kanghaeun@users.noreply.github.com>
2025-07-01 01:21:49 +0900

    전남대 FE 강하은 3단계 - 로그인 페이지 UI 만들기 (#150)
    * feat: react-icons 설치 및 적용
    
    * refactor: 공통 Text 컴포넌트 구현으로 타이포그래피 일관성 개선
    
    * feat: 로그인 페이지 구현
    
    * refactor: 폴더 구조 변경
    
    * feat: 로그인 성공 시 홈으로 리디렉션 구현
    
    * feat: 뒤로 가기 기능 구현
    
    * feat: 404 Not Found 페이지 및 잘못된 경로 대응 추가
    
    * feat: 실시간 선물랭킹 필터 상태를 URL 쿼리스트링으로 유지되도록 변경

---
+++ src/common/NavigationBar.tsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+import { IoIosArrowBack } from 'react-icons/io';
+import { LuUserRound } from 'react-icons/lu';
+import { useNavigate } from 'react-router-dom';
+import Text from '@/common/Text';
+
+const NavigationBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Layout>
+      <IoIosArrowBack
+        size={25}
+        onClick={() => {
+          navigate(-1);
+        }}
+      />
+      <Text fontSize="title1Bold" fontWeight="title2Bold">
+        선물하기
+      </Text>
+      <LuUserRound
+        size={25}
+        onClick={() => {
+          navigate(`/login`);
+        }}
+      />
+    </Layout>
+  );
+};
+
+export default NavigationBar;
+const Layout = styled.div`
+  box-sizing: border-box;
+  position: fixed;
+  padding: 0 8px 0 8px;
+  top: 0;
+  width: 100%;
+  max-width: 720px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 44px;
+  background-color: ${({ theme }) => theme.colors.gray00};
+`;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GiftHome from '@/pages/GiftHome';
+import Login from '@/pages/Login';
+import NotFound from '@/pages/Notfound';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<GiftHome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
