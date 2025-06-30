85a6d4c91b20d15255920b653fcd4bc5637ec0b6
shushuburger <03robin@naver.com>
2025-07-01 03:07:36 +0900

    refactor(text-banner): children 기반 구조로 개선
    - TextBanner 컴포넌트가 children을 받아 다양한 JSX 콘텐츠를 지원하도록 개선하였습니다.
    - 기존의 topText, bottomText 문자열 props를 제거하였습니다.
    - BannerSection.tsx도 이에 맞춰 <Banner>...</Banner> 형태로 수정하였습니다.

--- src/components/BannerSection.tsx
+++ src/components/BannerSection.tsx
@@ -1,12 +1,23 @@
-import TextBanner from '@/components/common/TextBanner';
+import Banner from '@/components/common/Banner';
+import styled from '@emotion/styled';
 
 const BannerSection = () => {
   return (
-    <TextBanner
-      topText="카카오테크 캠퍼스 3기여러분"
-      bottomText="프론트엔드 2단계 과제 화이팅! 🎉"
-    />
+    <Banner>
+      <TopText>카카오테크 캠퍼스 3기 여러분</TopText>
+      <BottomText>프론트엔드 2단계 과제 화이팅! 🎉</BottomText>
+    </Banner>
   );
 };
 
 export default BannerSection;
+
+const TopText = styled.p`
+  ${({ theme }) => theme.typography.label.label2Regular};
+  color: ${({ theme }) => theme.color.gray[700]};
+`;
+
+const BottomText = styled.p`
+  ${({ theme }) => theme.typography.label.label1Bold};
+  color: ${({ theme }) => theme.color.semantic.text.default};
+`;
