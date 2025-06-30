ff557e9ab2d176ed50f06bdeab6c5ea4d72e58d6
Chahyun Lee <163325051+chahyunlee@users.noreply.github.com>
2025-06-30 15:17:04 +0900

    feat: Not Found 페이지 구현

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,15 +2,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RouterPath } from "@/routes/path";
 import HomePage from "@/pages/Home/Home";
 import LoginPage from "@/pages/Login/Login";
+import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 
 const router = createBrowserRouter([
   {
     path: RouterPath.HOME,
     element: <HomePage />,
+    errorElement: <NotFoundPage />,
   },
   {
     path: RouterPath.LOGIN,
     element: <LoginPage />,
+    errorElement: <NotFoundPage />,
   },
 ]);
 
