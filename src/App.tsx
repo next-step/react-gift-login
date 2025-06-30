6b2d79a60a701b9af94fbd304b47529f05d2ba4c
jskim <jskim3936@naver.com>
2025-06-30 15:15:36 +0900

    feat: NotFound 페이지 생성 및 경로설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
