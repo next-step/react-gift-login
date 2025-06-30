ff24a62845a9de4df50eeebdac86485bcb36a9cb
eodudrepublic <eodudrepublic@gmail.com>
2025-06-30 11:58:00 +0900

    feat(style): RootLayout를 추가하여 max-width를 720px으로 제한 + 가운데 정렬

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx'
 import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from '@/styles/GlobalStyle';
 import { theme }   from '@/styles/theme'; 
+import RootLayout from '@/layout/RootLayout';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}> {/* 테마 컨텍스트 주입 */}
       <GlobalStyle />             {/* Reset + Pretendard 전역 적용 */}
-      <App />
+      <RootLayout>
+        <App />
+      </RootLayout>
     </ThemeProvider>
   </StrictMode>,
 )
