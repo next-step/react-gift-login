34b5955ca35bd9ca9f6eedefec05899b8e289973
Daeyoung Kim <eodudrepublic@gmail.com>
2025-06-30 23:12:08 +0900

    부산대 FE 김대영 - 2단계 : 선물하기 메인 페이지 UI 구현 (#160)
    * docs(readme): 구현할 기능 목록을 README.md에 정리
    
    * chore(template): 불필요한 vite demo assets 제거
    
    * feat(build): @ alias를 vite & tsconfig에 추가
    
    * chore(lint): prettier 추가
    
    * style(global): emotion 스타일 라이브러리 추가, reset css 적용, pretendard 폰트 적용
    
    * feat(style): Emotion 기반의 Reset CSS를 main.tsx(엔트리)에서 사용
    
    * docs(readme): 구현할 기능 목록을 README.md에 정리
    
    * feat(global): emotion에 Color, Typography 토큰을 설정
    
    * chore(types): Emotion 테마 토큰 보강용 전역 타입 선언 파일 emotion.d.ts 추가
    
    * feat(style): RootLayout를 추가하여 max-width를 720px으로 제한 + 가운데 정렬
    
    * feat(Home): 상단 네비게이션 바 영역 구현
    - 배경색 추가해야 함
    - 로그인 아이콘 수정해야 함
    
    * feat(Category): 카테고리 섹션 구현
    
    * feat(Category): 기타 섹션 (선물한 친구 선택, 카테캠 화이팅) UI 구현
    
    * feat(Trend): 실시간 급상승 선물랭킹 구현
    
    * style(global): run prettier, 주석 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { theme } from '@/styles/theme';
+import { ThemeProvider } from '@emotion/react';
+import App from './App.tsx';
+import GlobalStyle from '@/styles/GlobalStyle';
+import RootLayout from '@/layout/RootLayout';
+import 'material-icons/iconfont/material-icons.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      {' '}
+      {/* 테마 컨텍스트 주입 */}
+      <GlobalStyle /> {/* Reset + Pretendard 전역 적용 */}
+      <RootLayout>
+        {' '}
+        {/* max-width를 720px으로 제한, 중앙 정렬 */}
+        <App />
+      </RootLayout>
+    </ThemeProvider>
   </StrictMode>,
-)
+);
