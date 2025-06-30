0d6c4f949da72617d4ee577ae1b69845b744004d
jiiiwonyy <ji__won_22@naver.com>
2025-06-30 14:15:41 +0900

    feat: 로그인페이지 UI 구현

---
+++ src/shared/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '@/Pages/Home';
+import Login from '@/Pages/Login';
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
+export default Router;
