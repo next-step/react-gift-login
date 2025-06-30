7ef6eee39758e0b92b2f2b82c57b67e9561d4ba9
jiwonWP <bread0264@gmail.com>
2025-06-30 14:11:14 +0900

    feat: NotFound 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme/theme';
 import GiftMain from './pages/GiftMain';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
@@ -13,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<GiftMain />} />
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
