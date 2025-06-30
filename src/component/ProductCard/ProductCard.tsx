71b59e73f7c8406b459e4ed9b1b047e1d987b8e9
daaoooy <y020d226y@daum.net>
2025-06-30 16:31:07 +0900

    feat(product) : 상품 아이템 컴포넌트 분리
    - 재사용을 위해 TrendingGiftRanking 에 있던 프로덕트 컴포넌트를 분리함

---
+++ src/component/ProductCard/ProductCard.tsx
@@ -0,0 +1,46 @@
+import {
+  ProductTab,
+  ProductItem,
+  ProductImage,
+  Rank,
+  MoreButton,
+} from './ProductCard.styles'
+import type { Product } from '@/data/products'
+
+interface ProductCardProps {
+  products: Product[]
+  visibleCount: number
+  isExpanded: boolean
+  onProductSelect: (product: Product) => void
+  onToggleView: () => void
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({
+  products,
+  visibleCount,
+  isExpanded,
+  onProductSelect,
+  onToggleView,
+}) => {
+  return (
+    <>
+      <ProductTab>
+        {products.slice(0, visibleCount).map((item, index) => (
+          <ProductItem key={item.id} onClick={() => onProductSelect(item)}>
+            <Rank rank={index + 1}>{index + 1}</Rank>
+            <ProductImage src={item.imageURL} alt={item.name} />
+            <p>{item.brandInfo.name}</p>
+            <p>{item.name}</p>
+            <strong>{item.price.sellingPrice.toLocaleString()} 원</strong>
+          </ProductItem>
+        ))}
+      </ProductTab>
+
+      <MoreButton onClick={onToggleView}>
+        {isExpanded ? '접기' : '더보기'}
+      </MoreButton>
+    </>
+  )
+}
+
+export default ProductCard
