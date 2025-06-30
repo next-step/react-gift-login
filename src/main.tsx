6202eefe1168ffbd97036aa5cd4d924102511b82
wnhaoo <153691178+wnhaoo@users.noreply.github.com>
2025-06-30 14:08:36 +0900

    로그인 페이지 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
+import { RouterProvider } from 'react-router-dom';
 import { theme } from '@/styles/theme';
 import reset from '@/styles/reset';
-import App from './App';
+import { router } from './router';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Global styles={reset} />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import App from './App';
+import LoginPage from '@/pages/LoginPage';
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<LoginPage />} />
+    </>,
+  ),
+);
