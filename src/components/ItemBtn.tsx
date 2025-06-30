a459f81ca1e79bc6499f01d3cfdfbed4f6093b14
HanseKim <kimbird2002@naver.com>
2025-06-30 10:21:48 +0900

    refactor: import 뒤에 type 선언하기

--- src/components/ItemBtn.tsx
+++ src/components/ItemBtn.tsx
@@ -1,9 +1,9 @@
+import { ExpandBtn } from '@/styles/Item/ItemBtn.styles';
+
 type ItemBtnProps = {
   isExpanded: boolean;
   onClick: () => void;
 };
-
-import { ExpandBtn } from '@/styles/Item/ItemBtn.styles';
 
 function ItemBtn({ isExpanded, onClick }: ItemBtnProps) {
   return <ExpandBtn onClick={onClick}>{isExpanded ? '접기' : '더보기'}</ExpandBtn>;
