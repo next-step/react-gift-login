d292a03c9bac56800c7b3e1d145bfa462c41beda
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-07-01 11:33:50 +0900

    전남대 FE 최재원 - 2단계 : 선물하기 메인 페이지 UI 구현 (#194)
    * chore: 불필요한 파일 및 코드 정리
    
    * chore: prettier 설정 및 ignore 파일 생성
    
    * feat: emotion library 추가 및 reset css 적용
    
    * chore: 절대경로 alias 설정 추가
    
    * feat: 기본 폰트로 Pretendard 적용
    
    * docs: readme 수정 / 해야 할 일 정리
    
    * feat: 1단계 피드백 반영 / 패키지 설치
    
    * feat: 디자인 토큰 설정 / 타입 정의
    
    * feat: 상단바 구현 + react-icon 활용
    
    * feat: 카테고리 섹션 구현
    
    * feat: 메시지 섹션 구현 (2단계 과제 파이팅!)
    
    * feat: 선물할 친구 선택 섹션 구현
    
    * feat: trend Ranking Session 구현 / 스타일 및 컴포넌트 분리

---
+++ src/components/nav.tsx
@@ -0,0 +1,43 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { IoChevronBack } from 'react-icons/io5';
+import { FiUser } from 'react-icons/fi';
+
+const headerStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 56px;
+  padding: 0 16px;
+  border-bottom: 1px solid #eee;
+  background-color: #fff;
+  position: relative;
+`;
+
+const titleStyle = css`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const iconStyle = css`
+  font-size: 24px;
+  color: #333;
+  cursor: pointer;
+
+  &:hover {
+    color: #000;
+  }
+`;
+
+export default function Header() {
+  return (
+    <header css={headerStyle}>
+      <IoChevronBack css={iconStyle} aria-label="뒤로가기" />
+      <div css={titleStyle}>선물하기</div>
+      <FiUser css={iconStyle} aria-label="유저 아이콘" />
+    </header>
+  );
+}
