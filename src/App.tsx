162d4cbfba4d143bd8c6d5ed1ae89a9c4a5c0c39
sinyeowon <ioohyou@knu.ac.kr>
2025-07-01 11:30:53 +0900

    feat: createBrowserRouter 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from '@/pages/MainPage';
-import LoginPage from '@/pages/LoginPage';
-import Header from '@/components/Header';
-import { ROUTE_PATH } from '@/constants/routes';
-import NotFoundPage from '@/pages/NotFoundPage';
+import { RouterProvider } from 'react-router-dom';
+import { router } from '@/routes/router';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path={ROUTE_PATH.LOGIN} element={<LoginPage />} />
-        <Route path={ROUTE_PATH.HOME} element={<MainPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
