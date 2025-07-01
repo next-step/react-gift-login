b4df5465d469de4c479543eacd47e2f6639d9508
김성진 <gimseongjin@gimseongjin-ui-noteubug.local>
2025-07-01 13:48:54 +0900

    refactor: 이미지, 로고 common 파일로 분리

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,15 +1,13 @@
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 import HomeButton from '../components/common/BaseButton';
+import NotFoundImage from '../components/common/NotFoundImage';
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
   return (
     <Wrapper>
-      <img
-        src="https://gift-s.kakaocdn.net/dn/gift/webapp/images/m640/img_not_found.png"
-        alt="not found"
-      />
+      <NotFoundImage />
       <Title>페이지를 찾을 수 없습니다</Title>
       <Description>
         요청하신 페이지가 존재하지 않거나, 주소가 잘못되었습니다.
