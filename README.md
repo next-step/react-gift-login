906a11f6666951e4d80b4583b8174a63f4b7170c
DowanKim <83197376+dowankim1024@users.noreply.github.com>
2025-07-01 11:26:59 +0900

    Step3 (#164)
    * 1. 로그인 페이지 구현 및 라우터 구조 연결
    
    * 2. Not Found 페이지 구현 및 연결
    
    * 3. localstorage 기반 GiftRanking 상태 유지
    
    * 4. GiftRanking 리팩터링
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # react-gift-login
 
-1. 불필요한 파일, 코드 정리
-2. 절대경로로 import 할 수 있게 alias 설정
-3. Prettier 추가
-4. emotion 스타일 라이브러리 추가, reset css 적용
-5. 기본 폰트로 Pretendard 적용
+1. 로그인 페이지 구현 및 라우터 구조 연결
+2. Not Found 페이지 구현 및 연결
+3. localstorage 기반 GiftRanking 상태 유지
+4. GiftRanking 리팩터링
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,33 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '@/App';
 import { GlobalStyles } from '@/styles/global';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/styles/theme';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginPage from '@/pages/LoginPage';
+import PageLayout from '@/Layout/PageLayout';
+import HomePage from '@/pages/HomPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <PageLayout />,
+    children: [
+      { path: '', element: <HomePage /> },
+      { path: 'login', element: <LoginPage /> },
+      { path: '*', element : <NotFoundPage />}
+    ],
+  },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>,
 );
