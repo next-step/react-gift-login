243223e37f7e9d3307b27a77f50f80c3ec68e2c2
jabin00123 <133005789+jabin00123@users.noreply.github.com>
2025-07-01 21:43:12 +0900

    경북대 FE 윤자빈 2단계 - 선물하기 메인페이지 UI 구현하기  (#135)
    * 1. 불필요한 파일, 코드 정리
    
    * 2. 절대경로 alias 설정
    
    * 구현할 기능 목록 README에 정리
    
    * 3. Prettier 설정 추가
    
    * 4. emotion 설정 및 reset css 적용
    
    * 5. Pretendard 기본 폰트 적용
    
    * 1. Emotion 적용 & Pretendard 전역 폰트 설정
    
    * 2. __dirname
    
    * docs: 구현할 기능 readme 작성
    
    * 2. feat: emotion 토큰(color, typography) 설정
    
    * 3. feat: Mobile First Design 기반 Layout 구현
    
    * 4. feat: 상단 네비게이션 바 구현
    
    * 5. feat: 카테고리 섹션 구현
    
    * 6. feat: 선물한 친구 선택 구현
    
    * 7. feat: 카테캠 화이팅 UI(Banner) 구현

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const palette = {
+  primary: '#FFEB00', // 노란색 
+  gray100: '#F9F9F9',
+  gray200: '#EFEFEF',
+  gray400: '#C2C2C2',
+  gray900: '#1A1A1A',
+  white: '#FFFFFF',
+  black: '#000000',
+  blue: '#007AFF', // 파란색
+  sky: '#00A9FF', // 하늘색
+  sky100: '#E0F7FF', // 연한 하늘색
+  sky200: '#B3E5FC', // 더 연한 하늘색
+};
+
+export const typography = {
+  h1: { fontSize: '18px', fontWeight: 700, lineHeight: 1.4 },
+  body: { fontSize: '14px', fontWeight: 400, lineHeight: 1.5 },
+};
+//
