f2ebebd4f318e0a5a310bc4f30377c549aa648e4
jabin00123 <allisa1520@naver.com>
2025-07-02 13:50:26 +0900

    기프트페이지 오류 해결

---
+++ src/pages/GiftPage.tsx
@@ -0,0 +1,69 @@
+/** @jsxImportSource @emotion/react */
+import { useState } from 'react';
+import { Layout } from '@/components/Layout';
+import { NavBar } from '@/components/NavBar';
+import { FriendSelectBar } from '@/components/FriendSelectBar';
+import { CategoryGrid } from '@/components/CategoryGrid';
+import { Banner } from '@/components/Banner';
+import {
+  RankingTabs,
+  type GenderFilter,
+  type SortFilter,
+} from '@/components/RankingTabs';
+import { RankingGrid } from '@/components/RankingGrid';
+
+import {
+  rankingAll,
+  rankingFemale,
+  rankingMale,
+  rankingTeen,
+} from '@/data/rankings';
+
+type RankingItem = (typeof rankingAll)[number];
+
+export const GiftPage = () => {
+  const [gender, setGender] = useState<GenderFilter>('ALL');
+  const [sort, setSort] = useState<SortFilter>('GIVE');
+
+  const handleTab = (g: GenderFilter, s: SortFilter) => {
+    setGender(g);
+    setSort(s);
+  };
+
+  const base: RankingItem[] =
+    gender === 'FEMALE'
+      ? rankingFemale
+      : gender === 'MALE'
+        ? rankingMale
+        : gender === 'TEEN'
+          ? rankingTeen
+          : rankingAll;
+
+  type RankField = 'give' | 'want' | 'receive';
+  type RankStatItem = RankingItem & Record<RankField, number>; 
+
+  const sortFieldMap: Record<SortFilter, RankField> = {
+    GIVE: 'give',
+    WANT: 'want',
+    RECEIVE: 'receive',
+  };
+  const key = sortFieldMap[sort];
+
+  const list = ([...base] as RankStatItem[]).sort(
+    (a, b) => b[key] - a[key]
+  );
+
+
+  return (
+    <Layout>
+      <NavBar />
+      <FriendSelectBar />
+      <CategoryGrid />
+      <Banner />
+      <RankingTabs onChange={handleTab} />
+      <RankingGrid items={list} />
+    </Layout>
+  );
+};
+
+export default GiftPage;
