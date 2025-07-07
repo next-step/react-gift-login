8b3aa34cb4bace24e0cce6ea943ac8c7f2392d12
ppre1ude <cjh5110@naver.com>
2025-07-07 19:30:39 +0900

    feat: 선물 랭킹 필터 유지 기능 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/styles/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 );
