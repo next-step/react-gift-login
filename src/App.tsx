5501789db03b0b7e4b57b898f3a779a06a18bb2b
eodudrepublic <eodudrepublic@gmail.com>
2025-07-07 14:43:01 +0900

    feat(auth): 로그인 기능 구현 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
 import { Navbar } from '@/components/Navbar';
 import { CategorySection } from '@/components/CategorySection';
 import { ProductListSection } from '@/components/ProductListSection';
+import { LoginPage } from '@/pages/LoginPage';
+import RootLayout from '@/layout/RootLayout';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <CategorySection />
-        <ProductListSection />
-      </main>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <RootLayout>
+            <Navbar />
+            <main>
+              <CategorySection />
+              <ProductListSection />
+            </main>
+          </RootLayout>
+        }
+      />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
